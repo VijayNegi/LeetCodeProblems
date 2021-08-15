91121deb6f79986f29acd210ddf07dcce185647f
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2021-08-15 17:21:54 +0530

    8 ms

--- solutions/76. Minimum Window Substring.cpp
+++ solutions/76. Minimum Window Substring.cpp
@@ -1,3 +1,4 @@
+// https://leetcode.com/problems/minimum-window-substring/discuss/26808/Here-is-a-10-line-template-that-can-solve-most-'substring'-problems
 class Solution {
 public:
     string minWindow(string s, string t) {
