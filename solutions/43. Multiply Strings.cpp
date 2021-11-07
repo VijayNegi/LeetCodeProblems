ffe73be15fcbeb100ae5d3458377ede8af26cd2c
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2021-11-07 17:36:08 +0530

    8 ms

--- solutions/43. Multiply Strings.cpp
+++ solutions/43. Multiply Strings.cpp
@@ -1,31 +1,3 @@
-class Solution {
-public:
-    string multiply(string num1, string num2) {
-        
-        function<string(string,string)> bigAdd = [&](string num1,string num2)
-        {
-            int size1 = num1.size();
-            int size2 = num2.size();
-            string result;
-            int carry(0);
-            while( size1 && size2)
-            {
-                int num = (num1[size1-1] - '0') + (num2[size2-1]- '0') + carry;
-                carry = num/10;
-                num %=10;
-                result.push_back(num + '0');
-                --size1,--size2;
-            }
-            while( size1)
-            {
-                int num = (num1[size1-1] - '0') + carry;
-                carry = num/10;
-                num %=10;
-                result.push_back(num + '0');
-                --size1;
-            }
-            while( size2)
-            {
                 int num = (num2[size2-1] - '0') + carry;
                 carry = num/10;
                 num %=10;
@@ -68,4 +40,28 @@ public:
             return "0";
         return bigMul(num1,num2);
     }
+    
+    // Community solution
+    //https://leetcode.com/problems/multiply-strings/discuss/1563507/C%2B%2B-Simple-Easy-and-Short-Solution-Brief-Explanation
+    string multiply(string num1, string num2) {
+        if (num1 == "0" || num2 == "0") return "0";
+        
+        vector<int> res(num1.size()+num2.size(), 0);
+        
+        for (int i = num1.size()-1; i >= 0; i--) {
+            for (int j = num2.size()-1; j >= 0; j--) {
+                res[i + j + 1] += (num1[i]-'0') * (num2[j]-'0');
+                res[i + j] += res[i + j + 1] / 10;
+                res[i + j + 1] %= 10;
+            }
+        }
+        
+        int i = 0;
+        string ans = "";
+        while (res[i] == 0) i++;
+        while (i < res.size()) ans += to_string(res[i++]);
+        
+        return ans;
+    }
+​
 };
