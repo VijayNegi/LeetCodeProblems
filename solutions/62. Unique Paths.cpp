210c7ad4f5ae978e21c427e02e617fe6ce0c2c38
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2021-11-17 15:20:21 +0530

    0 ms

--- solutions/62. Unique Paths.cpp
+++ solutions/62. Unique Paths.cpp
@@ -13,7 +13,7 @@ public:
         return dp[m-1][n-1];
     }
     //space 2*n
-    int uniquePaths(int m, int n) {
+    int uniquePaths2(int m, int n) {
         vector<int> pre(n,1);
         vector<int> curr(n,1);
         
@@ -27,5 +27,17 @@ public:
         }
         
         return pre[n-1];
+    }
+    //space n
+    int uniquePaths(int m, int n) {
+        vector<int> curr(n,1);
+        
+        for(int i=1;i!=m;++i)
+        {
+            for(int j=1;j!=n;++j)
+                curr[j] += curr[j-1];
+        }
+        
+        return curr[n-1];
     }
 };
