5691731698a1b702dd0cd20b51db4c022c855eca
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2021-11-29 14:27:18 +0530

    92 ms

--- solutions/721. Accounts Merge.cpp
+++ solutions/721. Accounts Merge.cpp
@@ -58,3 +58,34 @@ public:
                     mail2acc[accounts[i][j]] = i;
             }
         }
+        
+        vector<vector<string>> res;
+        unordered_map<int, set<string>> acc2MailSet;
+        
+        // for(int i=0;i<count;++i)
+        // {
+        //     int par = disjoint.find(i);
+        //     for(int j=1;j<accounts[i].size();++j)
+        //     {
+        //          idMail[par].insert(accounts[i][j]);
+        //     }
+        // }
+        
+        for(auto [mail,acc]: mail2acc)
+        {
+            int par = disjoint.find(acc);
+            acc2MailSet[par].insert(mail);
+            
+        }
+        
+        for(auto [acc,s]: acc2MailSet)
+        {
+            vector<string> vec;
+            vec.push_back(accounts[acc][0]);
+            vec.insert(vec.end(),s.begin(),s.end());
+            res.push_back(vec);
+        }
+        
+        return res;
+    }
+};
