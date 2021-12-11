0f6cdecd93d31a95e3e01eaf822ff766801987be
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2021-12-11 14:28:05 +0530

    0 ms

--- solutions/878. Nth Magical Number.cpp
+++ solutions/878. Nth Magical Number.cpp
@@ -1,23 +1,3 @@
-typedef long long ll;
-const int mod = 1e9+7;
-class Solution {
-public:
-    int gcd(int a,int b)
-    {
-        if(b==0)
-            return a;
-        else
-            return gcd(b,a%b);
-    }
-    int nthMagicalNumber(int n, int a, int b) {
-        
-        int g = gcd(a,b);
-        //cout<<"g="<<g<<"\n";
-        int lower = min(a,b);
-        if(g==lower)
-            return ((ll)lower*n)%mod;
-        int lcm = (a*b)/g;
-        //cout<<"lcm="<<lcm<<"\n";
         int period = (lcm/a) + (lcm/b) - 1;
         //cout<<"period="<<period<<"\n";
         int m = n/period;
@@ -37,6 +17,40 @@ public:
         sort(begin(first),end(first));
         
         return ((ll)m*lcm + first[r])%mod;
+        
+            
+    }
+    // binary search, 0 ms
+    int nthMagicalNumber(int n, int a, int b) {
+        
+        int g = gcd(a,b);
+        //cout<<"g="<<g<<"\n";
+        int lower = min(a,b);
+        if(g==lower)
+            return ((ll)lower*n)%mod;
+        int lcm = (a*b)/g;
+        //cout<<"lcm="<<lcm<<"\n";
+        int period = (lcm/a) + (lcm/b) - 1;
+        //cout<<"period="<<period<<"\n";
+        int m = n/period;
+        int r = (n%period);
+        //cout<<"m="<<m<<"\n";
+        //cout<<"r="<<r<<"\n";
+        //vector<int> first{0};//(period+1,0);
+        
+        int l = 0;
+        int h = lcm;
+        
+        while(l<h)
+        {
+            int mid = l +(h-l)/2;
+            if(mid/a + mid/b - mid/lcm <r)
+                l = mid +1;
+            else
+                h = mid;
+        }
+        
+        return ((ll)m*lcm + h)%mod;
         
             
     }
