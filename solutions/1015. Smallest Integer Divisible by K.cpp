2602ab6cc93f895c7e987f7768b0d54d33c19fff
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2021-12-30 13:40:12 +0530

    4 ms

---
+++ solutions/1015. Smallest Integer Divisible by K.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int smallestRepunitDivByK(int k) {
+        if(k%2==0)
+            return -1;
+        
+        int len = 0;
+        unsigned long long n = 0;
+     
+        while(len < k+1 )
+        {
+            n = n*10+1;
+            ++len;
+​
+            if(n % k == 0)
+                return len;
+             n = n%k;
+        }
+       
+        return -1;
+    }   
+};
