f6e4ea4e52331f4e446f7a6311822308297f6dee
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2021-12-30 14:25:14 +0530

    3 ms

---
+++ solutions/191. Number of 1 Bits.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int hammingWeight1(uint32_t n) {
+        int count=0;
+        while(n)
+        {
+            if(n & 1)
+                ++count;
+            n = n>>1;
+        }
+        return count;
+    }
+    // only remove last set bit
+    int hammingWeight(uint32_t n) {
+        int count = 0;
+​
+        while (n) {
+            n &= (n - 1);
+            count++;
+        }
+​
+        return count;
+    }
+};
