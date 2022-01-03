f432a2d2f739b0db62c4e88b6fe4b370fbdbf4a2
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-01-03 10:44:08 +0530

    282 ms

---
+++ solutions/997. Find the Town Judge.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        vector<int> trustCount(n+1,0);
+        
+        for(auto& t: trust)
+        {
+            trustCount[t[0]]--; // cant be a judge
+            trustCount[t[1]]++;
+        }
+        
+        for(int i=1;i<=n;++i)
+            if(trustCount[i]==n-1)
+                return i;
+                
+        return -1;
+    }
+};
