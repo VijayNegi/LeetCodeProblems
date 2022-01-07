af2e54d924b6b64a81113de0a25cd3b75ca1f1c5
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-01-07 20:27:10 +0530

    109 ms

---
+++ solutions/217. Contains Duplicate.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    // nlogn 143 ms
+    bool containsDuplicate1(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        
+        for(int i=0;i<nums.size()-1;++i)
+            if(nums[i]==nums[i+1])
+                return true;
+        return false;
+    }
+    // O(n) 89 ms
+    bool containsDuplicate2(vector<int>& nums) {
+        unordered_set<int> seen;
+        
+        for(auto&  n:nums)
+        {
+            if(seen.count(n))
+                return true;
+            seen.insert(n);
+        }   
+        return false;
+    }
+    // one liner 109 ms
+    bool containsDuplicate(vector<int>& nums) {
+        return unordered_set<int>(nums.begin(), nums.end()).size() < nums.size();
+    }
+};
