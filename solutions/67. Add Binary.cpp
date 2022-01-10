63539f6f20070dd7f7f1aeb3626a10c19fc0220e
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-01-10 14:48:21 +0530

    0 ms

---
+++ solutions/67. Add Binary.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        string res;
+        
+        reverse(begin(a),end(a));
+        reverse(begin(b),end(b));
+        
+        int sum=0;
+        int i=0;
+        while(i<a.size() ||  i<b.size())
+        {
+            if(i<a.size())
+                sum += (a[i]=='1');
+            if(i<b.size())
+                sum += (b[i]=='1');
+            if(sum%2)
+                res.push_back('1');
+            else
+                res.push_back('0');
+            sum = sum>=2? 1:0;
+            ++i;
+        }
+        if(sum)
+            res.push_back('1');
+        
+        reverse(begin(res),end(res));
+        return res;
+    }
+};
