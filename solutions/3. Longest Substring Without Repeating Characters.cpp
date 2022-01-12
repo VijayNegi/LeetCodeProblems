8a7b6da9dc4c33f1a5dc6b6e6135d713fd2c61b2
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-01-12 20:06:17 +0530

    8 ms

--- solutions/3. Longest Substring Without Repeating Characters.cpp
+++ solutions/3. Longest Substring Without Repeating Characters.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     //sliding window : 12 ms
-    int lengthOfLongestSubstring(string s) {
+    int lengthOfLongestSubstring1(string s) {
         int n = s.size();
-        int l=0,r=n-1;
+        int l=0,r;
         int u = 0;
         vector<int> dp(256,0);
         for(r=0;r<n;++r)
-        {
+        {
             char c = s[r];
             ++dp[c];
             if(dp[c]==1)
@@ -18,8 +18,20 @@ public:
                 if(--dp[s[l++]] == 0)
                     --u;
             }
-        }
+        }
         
         return r-l;
+    }
+    // keep track of previous occurence. : 8 ms
+    int lengthOfLongestSubstring(string s) {
+        vector<int> dict(256, -1);
+        int maxLen = 0, start = -1;
+        for (int i = 0; i != s.length(); i++) {
+            if (dict[s[i]] > start)
+                start = dict[s[i]];
+            dict[s[i]] = i;
+            maxLen = max(maxLen, i - start);
+        }
+        return maxLen;
     }
 };
