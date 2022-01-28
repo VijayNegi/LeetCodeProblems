7b857a5e6c70ee554ec7179e1cf492e20d720969
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-01-28 17:14:00 +0530

    72 ms

--- solutions/211. Design Add and Search Words Data Structure.cpp
+++ solutions/211. Design Add and Search Words Data Structure.cpp
@@ -6,75 +6,65 @@ struct Trie
     bool end;
     Trie()
     {
-        for(int i=0;i<TRIE_LEN;++i)
-            next[i] = nullptr;
+        fill(next,next+ TRIE_LEN, nullptr);
         end = false;
     }
     
-    void insert(string& w)
+    void add(string s)
     {
         Trie* curr = this;
-        int idx = 0;
-        while(curr!=nullptr)
+        
+        for(auto c:s)
         {
-            if(idx == w.size())
-            {
-                curr->end = true;
-                break;
-            }
-            char c = w[idx];
-            c -='a';
-            if(!curr->next[c])
-                curr->next[c] = new Trie();
-            ++idx;
-            curr = curr->next[c];
-            
+            int idx = c-'a';
+            if(!(curr->next[idx]))
+                curr->next[idx] = new Trie();
+            curr = curr->next[idx];
         }
+        curr->end = true;
     }
     
-    bool search(string& w)
+    bool search(string& s)
     {
-        return search(w,0);
+        return search(s,0);
     }
     
-private:
-    bool search(string& w,int idx)
+    bool search(string& s, int idx)
     {
-        if(idx == w.size())
+        if(idx == s.size())
             return end;
-        char c = w[idx];
-        if( c == '.' )
+        
+        if(s[idx]!='.')
         {
-            for(int i = 0; i< TRIE_LEN; ++i)
-            {
-                if(next[i] && next[i]->search(w,idx+1))
-                        return true;
-            }
+            int i = s[idx]-'a';
+            if(next[i])
+               return next[i]->search(s,idx+1);
         }
         else
         {
-            c -= 'a';
-            if(next[c] && next[c]->search(w,idx+1))
-                return true;
+            for(int i=0;i<TRIE_LEN;++i)
+            {
+                if(next[i] && next[i]->search(s, idx+1))
+                    return true;
+            }
         }
-        
         return false;
     }
 };
 ​
 class WordDictionary {
-    Trie Dict;
+    Trie mDict;
 public:
     WordDictionary() {
         
     }
     
     void addWord(string word) {
-        Dict.insert(word);
+        mDict.add(word);
     }
     
     bool search(string word) {
-        return Dict.search(word);
+        return mDict.search(word);
     }
 };
 ​
