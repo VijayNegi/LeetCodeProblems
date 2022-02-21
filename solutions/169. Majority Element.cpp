3684fc97eb2daea94d0f3aa286da04c0df52fc77
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-02-21 19:54:11 +0530

    23 ms

---
+++ solutions/169. Majority Element.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int count=0;
+        int maj;
+        for(auto n:nums)
+        {
+            if(count==0)
+                maj = n;
+            count += (maj == n)? +1:-1;
+        }
+        return maj;
+    }
+};
