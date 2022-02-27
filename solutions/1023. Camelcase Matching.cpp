b8cb459732956fe12a29e65fa76954591d0ecec4
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-02-27 20:54:29 +0530

    0 ms

--- solutions/1023. Camelcase Matching.cpp
+++ solutions/1023. Camelcase Matching.cpp
@@ -6,7 +6,7 @@ public:
         int np = p.size();
         int is=0,ip=0;
         while(is<ns && ip<np)
-        {
+        {
             if(islower(s[is]) && (isupper(p[ip]) || s[is]!=p[ip]))
             {
                 ++is;
@@ -15,7 +15,7 @@ public:
             if(s[is]!=p[ip])
                 return false;
             ++is,++ip;
-        }
+        }
         while(is<ns)
         {
             if(isupper(s[is]))
@@ -26,7 +26,7 @@ public:
             return true;
         return false;
     }
-    vector<bool> camelMatch(vector<string>& queries, string pattern) {
+    vector<bool> camelMatch1(vector<string>& queries, string pattern) {
         int n = queries.size();
         vector<bool> result(n);
         for(int i=0;i<n;++i)
@@ -34,5 +34,20 @@ public:
             result[i] = camelMatch(queries[i],pattern);
         }
         return result;
+    }
+    
+    vector<bool> camelMatch(vector<string>& queries, string pattern) {
+        vector<bool> res;
+        for (string &query : queries) res.push_back(isMatch(query, pattern));
+        return res;
+​
+    }
+​
+    bool isMatch(string query, string pattern) {
+        int i = 0;
+        for (char & c : query)
+            if (i < pattern.length() && c == pattern[i]) i++;
+            else if (c < 'a') return false;
+        return i == pattern.length();
     }
 };
