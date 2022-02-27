3c784f87cf12e3c1f1e8e81f818bb6cf3e0b43fb
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-02-27 20:14:35 +0530

    1290 ms

---
+++ solutions/686. Repeated String Match.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    // std : 10ms
+    int repeatedStringMatch1(string a, string b) {
+        
+        string temp;
+        int bn = b.size();
+        int count=0;
+        while(temp.size() < bn)
+        {
+            temp+=a;
+            ++count;
+        }
+        
+        if(temp.find(b) != string::npos)
+            return count;
+        
+        temp += a;
+        ++count;
+        if(temp.find(b) != string::npos)
+            return count;
+        return -1;
+    }
+    // circuler match , better on space  1290 ms
+    int repeatedStringMatch(string A, string B) {
+    for (auto i = 0, j = 0; i < A.size(); ++i) {
+        for (j = 0; j < B.size() && A[(i + j) % A.size()] == B[j]; ++j);
+        if (j == B.size()) 
+            return (i + j - 1) / A.size() + 1;
+    }
+    return -1;
+}
+};
