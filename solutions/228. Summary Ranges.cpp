16baa2b76bef34eb4fc05b1b16de0d6fe4bdcbeb
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-02-28 15:28:28 +0530

    0 ms

---
+++ solutions/228. Summary Ranges.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<string> summaryRanges(vector<int>& nums) {
+        vector<string> result;
+        if(nums.size()==0)
+            return result;
+        int start=nums[0],end=nums[0];
+        for(int i=1;i<nums.size();++i)
+        {
+            if(nums[i] == end+1)
+                ++end;
+            else
+            {
+                string t;
+                t = to_string(start);
+                if(start!=end)
+                    t += "->"+to_string(end);
+                result.push_back(t);
+                start=end= nums[i];
+            }
+        }
+        string t;
+        t = to_string(start);
+        if(start!=end)
+            t += "->"+to_string(end);
+        result.push_back(t);
+        return result;
+    }
+};
