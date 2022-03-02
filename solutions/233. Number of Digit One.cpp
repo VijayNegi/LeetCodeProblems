53b94415048ad0197525360440aacfb9bf618f82
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-03-03 00:19:00 +0530

    Update 233. Number of Digit One.cpp

--- solutions/233. Number of Digit One.cpp
+++ solutions/233. Number of Digit One.cpp
@@ -1,54 +1,76 @@
-            int lmt = edge && i==limit;
-            if(i==1)
-            {
-                result += digit_dp(s,pos+1,sum+1, lmt);
-            }
-            else
-                result += digit_dp(s,pos+1,sum, lmt);
-        }
-        return dp[pos][sum][edge] = result;
-    }
-    //digit dp 2 states: 0 ms
-    // notes , Sum keeps track of numbers in the bondry of each digit. at end it will be of limit number
-     int countDigitOne2(int n) {
-        dp2 = vector(10,vector(82,-1));
-        string s = to_string(n);
-         int len = s.size();
-         int sum = 0;
-         int result = 0;
-         for(int i=0;i<len;++i)
-         {
-             int limit = s[i]-'0';
-             for(int j=0;j<limit;++j)
-             {
-                 result += digit_dp2(s,i+1,sum + (j==1));
-             }
-             if(limit==1)
-                 ++sum;
-         }
-        return result + sum;
-    }
-    
-    int digit_dp2(string s,int pos,int sum)
-    {
-        if(pos == s.size())
-            return sum;
-        if(dp2[pos][sum] != -1)
-            return dp2[pos][sum];
-        int limit = 9;
-        int result = 0;
-        for(int i=0;i<=limit;++i)
-        {
-            result += digit_dp2(s,pos+1,sum+(i==1));
-        }
-        return dp2[pos][sum] = result;
-    }
-    
-    // maths solution
-    int countDigitOne(int n) {
-        int ones = 0;
-        for (long long m = 1; m <= n; m *= 10)
-            ones += (n/m + 8) / 10 * m + (n/m % 10 == 1) * (n%m + 1);
-        return ones;
-    }   
+class Solution {
+    vector<vector<vector<int>>> dp;
+    vector<vector<int>> dp2;
+public:
+    // digit dp: 26 ms
+    int countDigitOne1(int n) {
+        dp = vector(10,vector(82,vector(2,-1)));
+        string s = to_string(n);
+        return digit_dp(s,0,0,1);
+    }
+    int digit_dp(string s,int pos,int sum,int edge)
+    {
+        if(pos == s.size())
+            return sum;
+        if(dp[pos][sum][edge] != -1)
+            return dp[pos][sum][edge];
+        int limit = 9;
+        if(edge==1)
+            limit = s[pos]-'0';
+        int result = 0;
+        for(int i=0;i<=limit;++i)
+        {
+            int lmt = edge && i==limit;
+            if(i==1)
+            {
+                result += digit_dp(s,pos+1,sum+1, lmt);
+            }
+            else
+                result += digit_dp(s,pos+1,sum, lmt);
+        }
+        return dp[pos][sum][edge] = result;
+    }
+    //digit dp 2 states: 0 ms
+    // notes , Sum keeps track of numbers in the bondry of each digit. at end it will be of limit number
+     int countDigitOne2(int n) {
+        dp2 = vector(10,vector(82,-1));
+        string s = to_string(n);
+         int len = s.size();
+         int sum = 0;
+         int result = 0;
+         for(int i=0;i<len;++i)
+         {
+             int limit = s[i]-'0';
+             for(int j=0;j<limit;++j)
+             {
+                 result += digit_dp2(s,i+1,sum + (j==1));
+             }
+             if(limit==1)
+                 ++sum;
+         }
+        return result + sum;
+    }
+    
+    int digit_dp2(string s,int pos,int sum)
+    {
+        if(pos == s.size())
+            return sum;
+        if(dp2[pos][sum] != -1)
+            return dp2[pos][sum];
+        int limit = 9;
+        int result = 0;
+        for(int i=0;i<=limit;++i)
+        {
+            result += digit_dp2(s,pos+1,sum+(i==1));
+        }
+        return dp2[pos][sum] = result;
+    }
+    
+    // maths solution
+    int countDigitOne(int n) {
+        int ones = 0;
+        for (long long m = 1; m <= n; m *= 10)
+            ones += (n/m + 8) / 10 * m + (n/m % 10 == 1) * (n%m + 1);
+        return ones;
+    }   
 };
