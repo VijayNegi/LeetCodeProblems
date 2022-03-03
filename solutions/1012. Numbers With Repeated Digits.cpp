451e38260bd821a892eb0ab6ad9839e8a8e005e6
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-03-04 00:53:48 +0530

    Update 1012. Numbers With Repeated Digits.cpp

--- solutions/1012. Numbers With Repeated Digits.cpp
+++ solutions/1012. Numbers With Repeated Digits.cpp
@@ -1,46 +1,71 @@
 class Solution {
-    vector<vector<int>> dp;
+    vector<vector<int>> dp;
 public:
-    int numDupDigitsAtMostN(int n) {
-        dp = vector(10,vector(1024,-1));
-        int last = 0;
-        string s = to_string(n);
-        int len = s.size();
-        bool flag = false; // if edge number are satisfy the condition
-        int result=0;
-        bool valid=false; // no leading zero for numbers with restriction.
-        for(int i=0;i<len;++i)
-        {
-            int limit = s[i]-'0';
-            for(int j=0;j<limit;++j)
-            {
-                int tvalid = valid || j >0;
-                if((tvalid && isSet(last,j)) || flag)
-                {
-                    int dLeft = s.size()-i -1;
-                    result += pow(10,dLeft);
-                }
-                else if(tvalid)
-                {
-                    int mask = setBit(last,j);
-                    result+=digit_dp(s,i+1,mask,tvalid);
-                }
-                else
-                    result+=digit_dp(s,i+1,0,tvalid);
-            }
-            if(isSet(last,limit))
-                flag = true;
-            valid = true;
-            last = setBit(last,limit);
-        }
-        return result+flag;
-    }
-    int digit_dp(string s,int pos,int last,bool valid) // valid = valid number not starting with zero
-    {
-        if(pos==s.size())
-            return 0;
-        if(dp[pos][last]!=-1)
-            return dp[pos][last];
-        int sum=0;
-        for(int i=0;i<10;++i)
-        {
+    int numDupDigitsAtMostN(int n) {
+        dp = vector(10,vector(1024,-1));
+        int last = 0;
+        string s = to_string(n);
+        int len = s.size();
+        bool flag = false; // if edge number are satisfy the condition
+        int result=0;
+        bool valid=false; // no leading zero for numbers with restriction.
+        for(int i=0;i<len;++i)
+        {
+            int limit = s[i]-'0';
+            for(int j=0;j<limit;++j)
+            {
+                int tvalid = valid || j >0;
+                if((tvalid && isSet(last,j)) || flag)
+                {
+                    int dLeft = s.size()-i -1;
+                    result += pow(10,dLeft);
+                }
+                else if(tvalid)
+                {
+                    int mask = setBit(last,j);
+                    result+=digit_dp(s,i+1,mask,tvalid);
+                }
+                else
+                    result+=digit_dp(s,i+1,0,tvalid);
+            }
+            if(isSet(last,limit))
+                flag = true;
+            valid = true;
+            last = setBit(last,limit);
+        }
+        return result+flag;
+    }
+    int digit_dp(string s,int pos,int last,bool valid) // valid = valid number not starting with zero
+    {
+        if(pos==s.size())
+            return 0;
+        if(dp[pos][last]!=-1)
+            return dp[pos][last];
+        int sum=0;
+        for(int i=0;i<10;++i)
+        {
+            int tvalid = valid || i>0;
+            if(tvalid && isSet(last,i)) // satisfy the condition, calculate count
+            {
+                int dLeft = s.size()-pos -1;
+                sum += pow(10,dLeft);
+            }
+            else if(tvalid)
+            {
+                int mask = setBit(last,i);
+                sum+=digit_dp(s,pos+1,mask,tvalid);
+            }
+            else // not a valid number till now
+                 sum+=digit_dp(s,pos+1,0,tvalid);
+        }
+        return dp[pos][last] = sum;
+    }
+    bool isSet(int number, int bit)
+    {
+        return number & (1<<bit);
+    }
+    int setBit(int number, int bit)
+    {
+        return number | (1<<bit);
+    }
+};
