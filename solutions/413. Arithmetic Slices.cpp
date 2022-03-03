5588a9e281d0f6c47a7f8611e05079499223d031
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-03-03 22:19:07 +0530

    3 ms

---
+++ solutions/413. Arithmetic Slices.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    // n2 solution
+    int numberOfArithmeticSlices1(vector<int>& nums) {
+       
+        int n = nums.size();
+        int result=0;
+        for(int i=0;i<n-1;++i)
+        {
+            int j=i+1;
+            int diff = nums[i+1]-nums[i];
+            while(j<n && nums[j]-nums[j-1] == diff)
+                ++j;
+            int count = j-i;
+            if(count>2)
+                result += count -2;
+        }
+        return result;
+    }
+    // o(n) solution
+    int numberOfArithmeticSlices(vector<int>& nums) {
+        int n = nums.size();
+        if (n<3){
+            return 0;
+        }
+        int ans = 0, count = 0;
+        for (int i=1;i<n-1;i++){
+            if (nums[i+1]-nums[i]==nums[i]-nums[i-1]){
+                count++;
+                ans +=count;
+            }
+            else{
+                count = 0;
+            }
+        }
+        return ans;
+    }
+​
+};
