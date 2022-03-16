db4dc3fe608e551dcb0b1c692bbbeb3b61e5305f
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-03-16 12:49:52 +0530

    4 ms

---
+++ solutions/946. Validate Stack Sequences.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
+        stack<int> stk;
+        int n = pushed.size();
+        int pu,po;
+        pu=po=0;
+        while(pu<pushed.size() && po<popped.size())
+        {
+            while(!stk.empty())
+            {
+                if(stk.top() == popped[po])
+                {
+                    stk.pop();
+                    po++;
+                }
+                else
+                    break;
+            }
+            stk.push(pushed[pu++]);  
+        }
+        
+        while(!stk.empty())
+        {
+                if(stk.top() == popped[po])
+                {
+                    stk.pop();
+                    po++;
+                }
+                else
+                    break;
+        }
+        
+        return stk.empty() && (pu>=n);
+    }
+};
