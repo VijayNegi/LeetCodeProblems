ddea4a8d2045f9e5284cee03f3099c0d2831f58b
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-03-17 00:20:48 +0530

    0 ms

--- solutions/91. Decode Ways.cpp
+++ solutions/91. Decode Ways.cpp
@@ -1,31 +1,22 @@
 class Solution {
 public:
     int numDecodings1(string s) {
-        vector<int> mem(s.size()+1,-1);
-        return dfs(s,0,mem);
+        vector<int> dp(s.size()+1,-1);
+        return numdp(s,0,dp);
     }
-    
-    int dfs(string s, int idx,vector<int>& mem)
+    int numdp(string& s,int i,vector<int>& dp)
     {
-        if(idx==s.size()) return 1;
-        if(s[idx] == '0') return 0;
-        if(idx==s.size()-1) return 1;
-​
-        if(mem[idx]>=0)
-            return mem[idx];
-            
-        int ways = 0;
-        ways+= dfs(s,idx+1,mem);
+        int n = s.size();
+        if(i==n) return 1;
+        if(s[i]=='0') return 0; // no number can start from 0
+        if(i== n-1) return 1;
+        if(dp[i]!=-1) return dp[i];
         
-        if(s[idx]=='1')
-            ways+= dfs(s,idx+2,mem);
-        else if(s[idx]=='2')
-        {
-            if(s[idx+1]<='6')
-                ways+= dfs(s,idx+2,mem);
-        }
-        mem[idx] = ways;
-        return ways;
+        int res = numdp(s,i+1,dp);
+        if( i < n-1 && ( s[i] =='1' || (s[i]=='2' && s[i+1]<'7')))
+           res += numdp(s,i+2,dp);
+        
+        return dp[i] = res;
     }
     //https://leetcode.com/problems/decode-ways/discuss/30451/Evolve-from-recursion-to-dp
     // dp with constant memory
@@ -39,5 +30,6 @@ public:
         }
         return s.empty()? 0 : p;   
     }
+    
     
 };
