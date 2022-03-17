d2877e99a514c90781a92015768e5c9c3b539233
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-03-17 23:40:47 +0530

    8 ms

--- solutions/76. Minimum Window Substring.cpp
+++ solutions/76. Minimum Window Substring.cpp
@@ -1,11 +1,10 @@
 // https://leetcode.com/problems/minimum-window-substring/discuss/26808/Here-is-a-10-line-template-that-can-solve-most-'substring'-problems
 class Solution {
 public:
-    string minWindow(string s, string t) {
+    string minWindow1(string s, string t) {
         int slen = s.size();
         int tlen = t.size();
         int dict[128]={0};
-        //fill_n(dict,128,0);
         
         for(int i=0;i<tlen;++i)
             ++dict[t[i]];
@@ -38,4 +37,19 @@ public:
             result = s.substr(begin,len);
         return result;
     }
+    
+    string minWindow(string s, string t) {
+        vector<int> map(128,0);
+        for(auto c: t) map[c]++;
+        int counter=t.size(), begin=0, end=0, d=INT_MAX, head=0;
+        while(end<s.size()){
+            if(map[s[end++]]-->0) counter--; //in t
+            while(counter==0){ //valid
+                if(end-begin<d)  d=end-(head=begin);
+                if(map[s[begin++]]++==0) counter++;  //make it invalid
+            }  
+        }
+        return d==INT_MAX? "":s.substr(head, d);
+    }
+​
 };
