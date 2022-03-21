ca6a0d497c747f54125b1e322d9f885c50d6c303
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-03-21 10:36:16 +0530

    4 ms

---
+++ solutions/763. Partition Labels.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+        int n = s.size();
+        vector<int> result,end(26,-1);
+        
+        for(int i=0;i<n;++i)
+            end[s[i]-'a'] = i;
+​
+        int l=0,r=0;
+        for(int i=0;i<n;++i)
+        {
+            r = max(r,end[s[i]-'a']);
+            if(r==i)
+            {
+                result.push_back(r-l+1);
+                l = r = i+1;
+            }
+        }
+        return result;
+    }
+};
