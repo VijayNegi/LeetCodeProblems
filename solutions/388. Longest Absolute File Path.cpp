c06093977c1ea018f754c49140939ce0097ff9be
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-03-22 13:56:24 +0530

    0 ms

---
+++ solutions/388. Longest Absolute File Path.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int lengthLongestPath(string input) {
+        stringstream ss(input);
+        int longpath = 0;
+        stack<int> stk;
+        string s;
+        while(!ss.eof())
+        {
+            getline(ss,s,'\n');
+            int tabs=0;
+            while(s[tabs]=='\t')
+                ++tabs;
+            s = s.substr(tabs);
+            
+            while(stk.size()> tabs)
+                stk.pop();
+            
+            if(s.find('.')!= string::npos) //found a file
+            {
+                int path = stk.empty()? 0:stk.top() + stk.size();
+                path += s.size();
+                if(path>longpath)
+                    longpath = path;
+            }
+            else // found a folder
+            {
+                int parent = stk.empty()?0:stk.top();
+                stk.push(parent + s.size());
+            }
+        }
+        
+        return longpath;
+    }
+};
