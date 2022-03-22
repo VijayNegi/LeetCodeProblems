391823bae1a99b2a90e43c51cfc894a439bb5c67
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-03-22 11:44:56 +0530

    39 ms

---
+++ solutions/1663. Smallest String With A Given Numeric Value.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string getSmallestString1(int n, int k) {
+        string s;
+        for(int i=n;i>0;--i)
+        {
+            int t = min(k-i,25);
+            s += 'a' + t;
+            k -=(t+1);
+        }
+        reverse(begin(s),end(s));
+        return s;
+    }
+    // another way: reverse fill
+    string getSmallestString(int n, int k) {
+        string res = string(n, 'a');
+        k -= n;
+        while (k > 0) {
+            res[--n] += min(25, k);
+            k -= min(25, k);
+        }
+        return res;
+    }
+};
