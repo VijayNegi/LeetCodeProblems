922bce5aad033bca5447b0662ffe654e22810ad9
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-03-26 11:09:54 +0530

    46 ms

---
+++ solutions/704. Binary Search.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    // using stl
+    int search1(vector<int>& nums, int target) {
+        auto it = lower_bound(begin(nums),end(nums),target);
+        if(it != nums.end())
+        {
+            if(*it==target)
+                return distance(nums.begin(),it);
+        }
+         return -1;   
+    }
+    // binary search
+    int search(vector<int>& nums, int target) {
+        int l=0,r=nums.size()-1,mid;
+        while(l<=r)
+        {
+            mid = l + (r-l)/2;
+            if(nums[mid]==target)
+                return mid;
+            else if(nums[mid]>target)
+                r = mid-1;
+            else
+                l = mid+1;
+        }
+        return -1;
+    }
+};
