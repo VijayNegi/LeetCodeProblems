1d7841effe04e9899bc7302388ed7bdece3d4d6e
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-03-29 15:03:13 +0530

    135 ms

---
+++ solutions/287. Find the Duplicate Number.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    // property of n natual numbers
+    int findDuplicate1(vector<int>& nums) {
+        int ans = -1;
+        
+        for(auto& a:nums)
+        {
+            if(nums[abs(a)]<0)
+            {
+                ans = abs(a);
+                break;
+            }
+            else
+                nums[abs(a)] = -1* nums[abs(a)];
+        }
+        for(auto& a:nums)
+            if(a<0)
+                a = -1*a;
+        
+        return ans;
+    }
+    int findDuplicate(vector<int>& nums) {
+        int slow(0),fast(0);
+        
+        while(true)
+        {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+            if(slow==fast)
+                break;
+        }
+        
+        slow = 0;
+        while(true)
+        {
+            slow = nums[slow];
+            fast = nums[fast];
+            if(slow==fast)
+                break;
+        }
+        return slow;
+    }
+};
