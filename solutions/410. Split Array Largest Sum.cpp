0491938c19ee3fb460996b24a66270a922e659af
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-04-01 01:43:33 +0530

    Update 410. Split Array Largest Sum.cpp

--- solutions/410. Split Array Largest Sum.cpp
+++ solutions/410. Split Array Largest Sum.cpp
@@ -1,71 +1,126 @@
-                    continue;
-                }
-​
-                // Otherwise, use the recurrence relation to determine the minimum largest subarray
-                // sum between currIndex and the end of the array with subarrayCount subarray remaining.
-                int minimumLargestSplitSum = INT_MAX;
-                for (int i = currIndex; i <= n - subarrayCount; i++) {
-                    // Store the sum of the first subarray.
-                    int firstSplitSum = prefixSum[i + 1] - prefixSum[currIndex];
-​
-                    // Find the maximum subarray sum for the current first split.
-                    int largestSplitSum = max(firstSplitSum, memo[i + 1][subarrayCount - 1]);
-​
-                    // Find the minimum among all possible combinations.
-                    minimumLargestSplitSum = min(minimumLargestSplitSum, largestSplitSum);
-​
-                    if (firstSplitSum >= minimumLargestSplitSum) {
-                        break;
-                    }
-                }
-​
-                memo[currIndex][subarrayCount] = minimumLargestSplitSum;
-            }
-        }
-        
-        return memo[0][m];
-    }
-    
-    // ***** binary search 0 ms
-    int minimumSubarraysRequired(vector<int>& nums, int maxSumAllowed) {
-        int currentSum = 0;
-        int splitsRequired = 0;
-        
-        for (int element : nums) {
-            // Add element only if the sum doesn't exceed maxSumAllowed
-            if (currentSum + element <= maxSumAllowed) {
-                currentSum += element;
-            } else {
-                // If the element addition makes sum more than maxSumAllowed
-                // Increment the splits required and reset sum
-                currentSum = element;
-                splitsRequired++;
-            }
-        }
-        
-        // Return the number of subarrays, which is the number of splits + 1
-        return splitsRequired + 1;
-    }
-    
-    int splitArray(vector<int>& nums, int m) {
-        // Find the sum of all elements and the maximum element
-        int sum = 0;
-        int maxElement = INT_MIN;
-        for (int element : nums) {
-            sum += element;
-            maxElement = max(maxElement, element);
-        }
-        
-        // Define the left and right boundary of binary search
-        int left = maxElement;
-        int right = sum;
-        int minimumLargestSplitSum = 0;
-        while (left <= right) {
-            // Find the mid value
-            int maxSumAllowed = left + (right - left) / 2;
-            
-            // Find the minimum splits. If splitsRequired is less than
-            // or equal to m move towards left i.e., smaller values
-            if (minimumSubarraysRequired(nums, maxSumAllowed) <= m) {
-                right = maxSumAllowed - 1;
-                minimumLargestSplitSum = maxSumAllowed;
+class Solution {
+public:
+    // top down dp 296ms
+    int splitArray1(vector<int>& nums, int m) {
+        int n = nums.size();
+        vector<vector<int>> mem(n+1,vector(m+1,-1));
+        function<int(int,int,int)> dfs = [&](int idx,int split,int sum)
+        {
+            if(mem[idx][split] != -1)
+                return mem[idx][split];
+            if(split==1)
+                return mem[idx][split] = sum;
+            int s = 0;
+            int result = INT_MAX;
+            for(int i=idx;i<=(n-split);++i)
+            {
+                s += nums[i];
+                int res = dfs(i+1,split -1,sum-s);
+                result = min(result,max(res,s));
+                if(s>result)
+                    break;
+            }
+            return mem[idx][split] = result;
+        };
+        int sum = accumulate(begin(nums),end(nums),0);
+        return dfs(0,m,sum);
+    }
+    
+    //**** bottom up 97 ms
+    
+    int memo[1001][51];
+    int splitArray2(vector<int>& nums, int m) {
+        int n = nums.size();
+        
+        // Store the prefix sum of nums array
+        vector<int> prefixSum(n + 1, 0);
+        for (int i = 0; i < n; i++) {
+            prefixSum[i + 1] = prefixSum[i] + nums[i];
+        }
+        
+        for (int subarrayCount = 1; subarrayCount <= m; subarrayCount++) {
+            for (int currIndex = 0; currIndex < n; currIndex++) {
+               // Base Case: If there is only one subarray left, then all of the remaining numbers
+               // must go in the current subarray. So return the sum of the remaining numbers.
+                if (subarrayCount == 1) {
+                    memo[currIndex][subarrayCount] = prefixSum[n] - prefixSum[currIndex];
+                    continue;
+                }
+
+                // Otherwise, use the recurrence relation to determine the minimum largest subarray
+                // sum between currIndex and the end of the array with subarrayCount subarray remaining.
+                int minimumLargestSplitSum = INT_MAX;
+                for (int i = currIndex; i <= n - subarrayCount; i++) {
+                    // Store the sum of the first subarray.
+                    int firstSplitSum = prefixSum[i + 1] - prefixSum[currIndex];
+
+                    // Find the maximum subarray sum for the current first split.
+                    int largestSplitSum = max(firstSplitSum, memo[i + 1][subarrayCount - 1]);
+
+                    // Find the minimum among all possible combinations.
+                    minimumLargestSplitSum = min(minimumLargestSplitSum, largestSplitSum);
+
+                    if (firstSplitSum >= minimumLargestSplitSum) {
+                        break;
+                    }
+                }
+
+                memo[currIndex][subarrayCount] = minimumLargestSplitSum;
+            }
+        }
+        
+        return memo[0][m];
+    }
+    
+    // ***** binary search 0 ms
+    int minimumSubarraysRequired(vector<int>& nums, int maxSumAllowed) {
+        int currentSum = 0;
+        int splitsRequired = 0;
+        
+        for (int element : nums) {
+            // Add element only if the sum doesn't exceed maxSumAllowed
+            if (currentSum + element <= maxSumAllowed) {
+                currentSum += element;
+            } else {
+                // If the element addition makes sum more than maxSumAllowed
+                // Increment the splits required and reset sum
+                currentSum = element;
+                splitsRequired++;
+            }
+        }
+        
+        // Return the number of subarrays, which is the number of splits + 1
+        return splitsRequired + 1;
+    }
+    
+    int splitArray(vector<int>& nums, int m) {
+        // Find the sum of all elements and the maximum element
+        int sum = 0;
+        int maxElement = INT_MIN;
+        for (int element : nums) {
+            sum += element;
+            maxElement = max(maxElement, element);
+        }
+        
+        // Define the left and right boundary of binary search
+        int left = maxElement;
+        int right = sum;
+        int minimumLargestSplitSum = 0;
+        while (left <= right) {
+            // Find the mid value
+            int maxSumAllowed = left + (right - left) / 2;
+            
+            // Find the minimum splits. If splitsRequired is less than
+            // or equal to m move towards left i.e., smaller values
+            if (minimumSubarraysRequired(nums, maxSumAllowed) <= m) {
+                right = maxSumAllowed - 1;
+                minimumLargestSplitSum = maxSumAllowed;
+            } else {
+                // Move towards right if splitsRequired is more than m
+                left = maxSumAllowed + 1;
+            }
+        }
+        
+        return minimumLargestSplitSum;
+    }
+};
