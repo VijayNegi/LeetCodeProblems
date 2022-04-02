f5c0eb247fc40fcb788245095ebb67d53c57f1c9
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-04-02 14:13:39 +0530

    57 ms

--- solutions/680. Valid Palindrome II.cpp
+++ solutions/680. Valid Palindrome II.cpp
@@ -1,36 +1,30 @@
 class Solution {
 public:
     bool validPalindrome1(string s) {
-        int n = s.size();
-        int i=0,j=n-1;
-        int k=1;
-        while(i<j)
-        {
-            if(s[i]==s[j])
-            {
-                ++i;--j;
-            }
-            else if(k>0)
-                ++i,--k;
-            else 
+        int l=0,r=s.size()-1;
+        while(l<r)
+            if(s[l]==s[r])
+                ++l,--r;
+            else
                 break;
-                
-        }
-        if(i>=j)
+        if(l>=r)
             return true;
-        i=0,j=n-1,k=1;
-        while(i<j)
-        {
-            if(s[i]==s[j])
-            {
-                ++i;--j;
-            }
-           else if(k>0)
-                --j,--k;
-            else 
+        int l1=l+1;
+        int r1=r;
+        while(l1<r1)
+            if(s[l1]==s[r1])
+                ++l1,--r1;
+            else
                 break;
-        }
-        if(i>=j)
+        if(l1>=r1)
+            return true;
+        l1=l;r1=r-1;
+        while(l1<r1)
+            if(s[l1]==s[r1])
+                ++l1,--r1;
+            else
+                break;
+        if(l1>=r1)
             return true;
         return false;
     }
