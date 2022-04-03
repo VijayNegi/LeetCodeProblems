3171c5f7ff0e4f9a43558d619bb97da7bd7ed841
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-04-03 16:07:15 +0530

    0 ms

--- solutions/31. Next Permutation.cpp
+++ solutions/31. Next Permutation.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     // 7 ms
-    void nextPermutation1(vector<int>& nums) {
+    void nextPermutation(vector<int>& nums) {
         int n = nums.size();
-        int i=n-2;
-        for(i=n-2;i>=0;--i)
-        {
-            if(nums[i]<nums[i+1])
+        int i = n-2;
+        for(;i>=0;--i)
+            if(nums[i+1]>nums[i])
             {
                 int j=i+1;
                 for(;j<n;++j)
@@ -15,15 +14,14 @@ public:
                 swap(nums[i],nums[j-1]);
                 break;
             }
-        }
         reverse(begin(nums)+i+1,end(nums));
     }
     // 3 ms
     void nextPermutation2(vector<int>& nums) {
       std::next_permutation(nums.begin(), nums.end());
     }
-    // 4 ms : some more STL 
-    void nextPermutation(vector<int>& nums) {
+    // some more STL
+    void nextPermutation3(vector<int>& nums) {
       auto dest = std::is_sorted_until(nums.rbegin(), nums.rend());
       if (dest != nums.rend()) {
         auto target = std::upper_bound(nums.rbegin(), dest, *dest);
