00d918b367bb5d54859962592742f17d56984484
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-04-05 12:59:20 +0530

    109 ms

--- solutions/11. Container With Most Water.cpp
+++ solutions/11. Container With Most Water.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
-    // 167 ms
     int maxArea(vector<int>& height) {
-        int maxWater = 0;
+        int result=0;
         int n = height.size();
-        int l = 0;
-        int r = n-1;
-        int containerHeightSeen = 0;
+        int l=0,r=n-1,maxh=0;
         while(l<r)
         {
             int h = min(height[l],height[r]);
-            int water = h*(r-l);
-            maxWater = max(maxWater,water);
-            containerHeightSeen = max(h,containerHeightSeen);
+            result = max(result,h*(r-l));
+            maxh = max(maxh,h);
             if(height[l]<height[r])
-                while(l<r && height[l]<=containerHeightSeen) ++l;
+                 while(l<r && height[l]<=maxh) ++l; 
             else
-                while(r>l && height[r]<=containerHeightSeen) --r;
+                 while(l<r && height[r]<=maxh) --r; 
+​
         }
-        
-        return maxWater;
+        return result;
     }
 };
