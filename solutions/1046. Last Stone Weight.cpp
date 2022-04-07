1b84e19f75663335bdf396c9bf4bb3e106f975bb
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-04-07 16:23:54 +0530

    0 ms

---
+++ solutions/1046. Last Stone Weight.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> pq(begin(stones),end(stones));
+        while(pq.size()>1)
+        {
+            int s1 = pq.top(); pq.pop();
+            int s2 = pq.top(); pq.pop();
+            if(s1>s2)
+                pq.push(s1-s2);    
+        }
+        return pq.size()? pq.top():0;
+    }
+};
