ede32df06e455e1cca91e7cf3b169ab501c5033d
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-05-04 00:39:33 +0530

    36 ms

--- solutions/581. Shortest Unsorted Continuous Subarray.cpp
+++ solutions/581. Shortest Unsorted Continuous Subarray.cpp
@@ -1,10 +1,37 @@
 class Solution {
 public:
-// Think of problem as - In ideal sorted array. 
-// Every element from right to left will be smaller.
-// Every element from left to right will be larger.
-// finding atual position of Min by going right to left you will get start.
-// finding atual position of max by going left to right you will get end.
+    int findUnsortedSubarray1(vector<int>& nums) {
+        int n = nums.size();
+        if(n==1)
+            return 0;
+         int l=n-1,r=0;
+        int low = nums[n-1];
+        int high = nums[0];
+        
+        for(int i=1;i<n;++i)
+        {
+            if(nums[i]>=high)
+                high = nums[i];
+            else
+                r=i;
+        }
+        
+        for(int i=n-2;i>=0;--i)
+        {
+            if(nums[i]<=low)
+                low = nums[i];
+            else
+                l=i;
+        }
+        if(l<r)
+            return r-l+1;
+        return 0;
+    }
+    // Think of problem as - In ideal sorted array. 
+    // Every element from right to left will be smaller.
+    // Every element from left to right will be larger.
+    // finding atual position of Min by going right to left you will get start.
+    // finding atual position of max by going left to right you will get end.
     int findUnsortedSubarray(vector<int>& nums) {
         int n = nums.size();
         int minV = nums[n-1];
