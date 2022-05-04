173f110ed760a9314809d24658e033b31b415858
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-05-05 00:29:18 +0530

    221 ms

---
+++ solutions/1679. Max Number of K-Sum Pairs.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    // multiset : 393ms
+    // Note : Multiset erase with key removes all keys
+    int maxOperations1(vector<int>& nums, int k) {
+        multiset<int> ms;
+        int result=0;
+        for(int n:nums)
+        {
+            auto it = ms.find(k-n);
+            if(it != ms.end())
+            {
+                ms.erase(it);
+                ++result;
+            }
+            else
+                ms.insert(n);
+        }
+        return result;
+    }
+    // unordered_map: 221 ms
+    int maxOperations(vector<int>& nums, int k) {
+        unordered_map<int,int> mp;
+        int result=0;
+        for(int n:nums)
+        {
+            if(mp[k-n]>0)
+            {
+                --mp[k-n];
+                ++result;
+            }
+            else
+                ++mp[n];
+        }
+        return result;
+    }
+};
