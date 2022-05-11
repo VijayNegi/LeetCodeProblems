c6a7422c2f536dcadb6c091d01b155db1d16f7f7
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-05-11 12:38:14 +0530

    0 ms

---
+++ solutions/1641. Count Sorted Vowel Strings.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    // dp
+    int countVowelStrings1(int n) {
+        int k=5;
+        int result=1;
+        vector<vector<int>> dp(n+1,vector(k+1,0));
+        function<int(int,int)> dfs = [&](int v,int pos){
+            if(pos==0)
+                return 1;
+            if(dp[pos][v]!=0)
+                return dp[pos][v];
+            int result = 0;
+            for(int i=1;i<=v;++i)
+                result += dfs(i,pos-1);
+            return dp[pos][v] = result;
+        };
+        result = dfs(k,n);
+        return result;
+    }
+    // combination version
+    int countVowelStrings(int n) {
+//                a  e  i  o  u
+//     initialy: {1, 1, 1, 1, 1}   
+//      n == 1 : {5, 4, 3, 2, 1}   
+//      n == 2 : {15,10,6, 3, 1}   
+//      n == 3 : {35,20,10,4, 1}   
+//      
+        int k=4;
+        int permutation[] = {1,1,1,1,1};
+        
+        for(int i = 0 ;i < n; i++)
+            for(int j = k-1; j>=0; j--)
+                permutation[j] += permutation[j+1] ;
+           
+        return permutation[0];
+    }
+};
