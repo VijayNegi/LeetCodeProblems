902e171c4674f8d6ff1c0800dcfefef371b22e85
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-07-06 00:37:05 +0530

    316 ms

--- solutions/128. Longest Consecutive Sequence.cpp
+++ solutions/128. Longest Consecutive Sequence.cpp
@@ -1,3 +1,4 @@
+struct DSU {
     vector<int> parent;
     vector<int> size;
     int maxSize=1;
@@ -5,15 +6,15 @@
         iota(parent.begin(),parent.end(),0);
     }
     
-    int getParent(int idx) {
+    int find(int idx) {
         if(parent[idx]==idx)
             return idx;
-        return parent[idx] = getParent(parent[idx]);
+        return parent[idx] = find(parent[idx]);
     }
     
     void merge(int idx1,int idx2) {
-        int p1 = getParent(idx1);
-        int p2 = getParent(idx2);
+        int p1 = find(idx1);
+        int p2 = find(idx2);
         if(p1==p2)
             return;
         parent[p2] = p1;
@@ -42,34 +43,3 @@ public:
             if(idx.count(nums[i]-1)>0) {
                 numset.merge(idx[nums[i]],idx[nums[i]-1]);
             }
-        }
-        return numset.maxSize;
-    }
-    // find smallest number in sequence: 1033ms
-    int longestConsecutive2(vector<int>& nums) {
-        
-        unordered_set us(nums.begin(),nums.end());
-        int result = 0;
-        for(auto n:nums) {
-            if(us.count(n-1)==0) {
-                int res = 1;
-                while(us.count(++n)==1)
-                    ++res;
-                result = max(res,result);
-            }
-        }
-        return result;
-    }
-    // another tricky solution: 316 ms
-    // update sequence extrimities
-    int longestConsecutive(vector<int>& nums) {
-        unordered_map<int, int> m;
-        int r = 0;
-        for (int i : nums) {
-            if (m[i]) continue;
-            r = max(r, m[i] = m[i + m[i + 1]] = m[i - m[i - 1]] = m[i + 1] + m[i - 1] + 1);
-        }
-        return r;
-        
-        }
-};
