29b00fbe1c62735d4d46b20ad72c28ceedd6e785
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-07-07 00:00:58 +0530

    4 ms

---
+++ solutions/509. Fibonacci Number.cpp
@@ -0,0 +1,42 @@
+class Solution {
+    vector<int> mem;
+public:
+    Solution():mem(31,0) {
+    }
+    // recursion
+    int fib1(int n) {
+        if(n<2)
+            return n;
+        if(mem[n]!=0)
+            return mem[n];
+        return mem[n] = fib(n-1)+fib(n-2);
+    }
+    // iteration
+    int fib2(int n) {
+        std::vector<int> dp(n+2,-1);
+        dp[0]=0;
+        dp[1]=1;
+        int i=1;
+        while(++i<=n) {
+            dp[i]=dp[i-2]+dp[i-1];
+        }
+        return dp[n];
+    }
+    // fast doubling
+    pair<int, int> fib_helper (int n) {
+        if (n == 0)
+            return {0, 1};
+        auto p = fib_helper(n >> 1);
+        int c = p.first * (2 * p.second - p.first);
+        int d = p.first * p.first + p.second * p.second;
+        if (n & 1)
+            return {d, c + d};
+        else
+            return {c, d};
+    }
+    int fib(int n) {
+        auto res = fib_helper(n);
+        return res.first;
+    }
+​
+};
