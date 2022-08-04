8bc847292222a2b85f3fa65e26397b5cea00ccce
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-08-05 00:36:29 +0530

    0 ms

---
+++ solutions/858. Mirror Reflection.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    
+    int gcd(int a,int b) {
+        if(b==0)
+            return a;
+        return gcd(b,a%b);
+    }
+    
+    int mirrorReflection(int p, int q) {
+        int lcm = (p*q)/gcd(p, q); // calculating lcm using gcd
+        int m = lcm/p;
+        int n = lcm/q;
+        if(n%2==0) return 2;
+        if(m%2==0) return 0;
+        return 1;
+​
+    }
+};
