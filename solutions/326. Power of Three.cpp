0a851bf59f06fd9716bbc41c4959066d6ab30b48
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-08-24 09:18:58 +0530

    31 ms

---
+++ solutions/326. Power of Three.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isPowerOfThree1(int n) {
+        if(n<=0)
+            return false;
+        int k=1;
+        while(k<=n) {
+            if(k==n)
+                return true;
+            k *=3;
+        }
+        return false;
+    }
+    bool isPowerOfThree2(int n) {
+        if(n<1)
+            return false;
+        
+        while(n%3==0) {
+           n/=3;
+        }
+        return n==1;
+    }
+    bool isPowerOfThree(int n) {
+        return n > 0 && 1162261467 % n == 0;
+    }
+};
+​
