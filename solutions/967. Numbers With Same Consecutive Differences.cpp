874d6c90ec7b21829bdedc9013406bafab9383c2
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-09-03 13:58:23 +0530

    3 ms

--- solutions/967. Numbers With Same Consecutive Differences.cpp
+++ solutions/967. Numbers With Same Consecutive Differences.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     // self : 9ms
-    vector<int> numsSameConsecDiff(int n, int k) {
+    vector<int> numsSameConsecDiff1(int n, int k) {
         vector<int> result;
         function<void(string)> dfs = [&](string s) {
             if(s.size()==n){
@@ -23,5 +23,21 @@ public:
             dfs(to_string(i));
         }
         return result;
+    }
+    // 3 ms
+    vector<int> numsSameConsecDiff(int N, int K) {
+        vector<int> cur = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        for (int i = 2; i <= N; ++i) {
+            vector<int> cur2;
+            for (int x : cur) {
+                int y = x % 10;
+                if (y + K < 10)
+                    cur2.push_back(x * 10 + y + K);
+                if (K > 0 && y - K >= 0)
+                    cur2.push_back(x * 10 + y - K);
+            }
+            cur = cur2;
+        }
+        return cur;
     }
 };
