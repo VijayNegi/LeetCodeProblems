4a5f7402cd361f1761b375907f092fd8d29a8f7d
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-09-13 10:12:37 +0530

    19 ms

---
+++ solutions/393. UTF-8 Validation.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool validUtf8(vector<int>& data) {
+        int n = data.size();
+        int dbytes = 1<<7;
+        int mask = 1<<7 | 1<<6;
+        int i=0;
+        while(i<n) {
+            int k = 0;
+            int p = data[i++];
+            for(int j=7;j>0;--j) // could have used bitset
+                if(p & (1<<j) )
+                    ++k;
+                else break;
+            if(k>4 || k==1) return false;
+            if((i+k-2)>=n && k>0)
+                return false;
+            for(int j=1;j<k;++j) {
+                int m = data[i++] & mask;
+                if(m != dbytes)
+                    return false;
+            }
+        }
+        return true;
+    }
+};
