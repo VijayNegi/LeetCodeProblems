1f7711aa0f5f927b3552529870aaa41d44bc2e68
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-09-21 22:23:36 +0530

    219 ms

--- solutions/985. Sum of Even Numbers After Queries.cpp
+++ solutions/985. Sum of Even Numbers After Queries.cpp
@@ -7,8 +7,7 @@ public:
             if(!(n & 1))  sum += n;
         vector<int> result;
         for(auto& q:queries){
-            int idx = q[1];
-            int val = q[0];
+            int idx = q[1], val = q[0];
             if(!(nums[idx] & 1))  sum -= nums[idx];
             nums[idx]+= val;
             if(!(nums[idx] & 1))  sum += nums[idx];
