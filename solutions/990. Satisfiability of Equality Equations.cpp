86c78f45d53f189c84196afc0e0aafb891da25f6
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-09-26 10:44:09 +0530

    11 ms

---
+++ solutions/990. Satisfiability of Equality Equations.cpp
@@ -0,0 +1,40 @@
+struct DSU
+{
+    vector<int> parent;
+    DSU(int n):parent(n)
+    {
+        iota(begin(parent), end(parent), 0);  // initializes each node's parent to be itself - fills as [0,1,2,3,...,n]
+    }
+    int find(int x)
+    {
+        if(parent[x] == x)          // x is itself the parent of the component that it belongs to
+            return x;
+        return parent[x] = find(parent[x]); // update parent of x before returning for each call -path compression
+​
+    } 
+    void unionsets(int x, int y)
+    {
+        int xx = find(x);
+        int yy = find(y);
+        if(xx == yy)
+            return;     // both belong to same component
+        parent[yy] = xx;
+    }
+};
+class Solution {
+public:
+    // DSU
+    bool equationsPossible(vector<string>& equations) {
+        DSU ds(26);
+        for(auto e:equations) {
+            if(e[1] == '=') 
+                ds.unionsets(e[0]-'a', e[3]-'a');
+        }
+        for(auto e:equations) {
+            if(e[1] == '!') 
+                if(ds.find(e[0]-'a') == ds.find(e[3]-'a'))
+                    return false;
+        }
+        return true;
+    }
+};
