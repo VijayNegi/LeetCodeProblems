66615680afbfb7df4d66b88771717aa77fb4f391
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-09-27 23:53:07 +0530

    87 ms

--- solutions/838. Push Dominoes.cpp
+++ solutions/838. Push Dominoes.cpp
@@ -3,9 +3,9 @@
 class Solution {
 public:
     // old solution
-    string pushDominoes(string dominoes) {↔}
+    string pushDominoes1(string dominoes) {↔}
     // new 
-    string pushDominoes1(string D) {
+    string pushDominoes2(string D) {
         int n = D.size();
         vector<int> forces(n,0);
 ​
@@ -31,5 +31,24 @@ public:
         for (int f: forces)
             result.push_back(f > 0 ? 'R' : f < 0 ? 'L' : '.');
         return result;
+    }
+    // https://leetcode.com/problems/push-dominoes/discuss/132332/JavaC%2B%2BPython-Two-Pointers
+    string pushDominoes(string d) {
+        d = 'L' + d + 'R';
+        string res = "";
+        for (int i = 0, j = 1; j < d.length(); ++j) {
+            if (d[j] == '.') continue;
+            int middle = j - i - 1;
+            if (i > 0)
+                res += d[i];
+            if (d[i] == d[j])
+                res += string(middle, d[i]);
+            else if (d[i] == 'L' && d[j] == 'R')
+                res += string(middle, '.');
+            else
+                res += string(middle / 2, 'R') + string(middle % 2, '.') + string(middle / 2, 'L');
+            i = j;
+        }
+        return res;
     }
 };
