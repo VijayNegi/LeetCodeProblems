405e88de3a9bd4358049f52b65af00d198a50087
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-09-29 09:49:22 +0530

    82 ms

---
+++ solutions/658. Find K Closest Elements.cpp
@@ -0,0 +1,33 @@
+        vector<int> findClosestElements1(vector<int>& arr, int k, int x) {
+        
+        int l=0,r=k-1;
+        int maxdis = max (abs(arr[l]-x), abs(arr[r]-x));
+        for(int i=k;i<arr.size();++i)
+        {
+            if(arr[i]-x>=maxdis)
+                break;
+            
+            if(abs(arr[i]-x) < maxdis)
+            {
+                r=i;
+                l=i-k+1;
+                maxdis = max(abs(arr[l]-x), abs(arr[r]-x));
+            }
+           
+        }
+        vector<int> result(begin(arr)+l,begin(arr)+r+1);
+        return result;
+    }
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        int left=0,right=arr.size()-k;
+        while(left<right)
+        {
+            int mid = (left+right)/2;
+            if(x-arr[mid] > arr[mid+k]-x)
+                left=mid+1;
+            else
+                right = mid;
+        }
+        return vector<int>(arr.begin()+left,arr.begin()+left+k);
+    }
+};
