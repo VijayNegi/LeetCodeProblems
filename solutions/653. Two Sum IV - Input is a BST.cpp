132b7331229e142ff4e0f40e8aa49062c837ef5d
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-10-09 12:30:19 +0530

    114 ms

--- solutions/653. Two Sum IV - Input is a BST.cpp
+++ solutions/653. Two Sum IV - Input is a BST.cpp
@@ -10,18 +10,34 @@
  * };
  */
 class Solution {
-    unordered_map<int,int> hash;
+    unordered_set<int> hash;
 public:
+    bool findTarget1(TreeNode* root, int k) {
+        unordered_map<int,int> seen;
+        stack<TreeNode*> stk;
+        function<void(TreeNode*)> push_left = [&](TreeNode* node) {
+            while(node) {
+                stk.push(node);
+                node = node->left;
+            }
+        };
+        push_left(root);
+        while(!stk.empty()) {
+            TreeNode* node = stk.top();
+            stk.pop();
+            push_left(node->right);
+            if(seen.count(k-node->val))
+                return true;
+            seen[node->val] = 1;
+        }
+        return false;
+    }
     bool findTarget(TreeNode* root, int k) {
-        if(!root)
-            return false;
-        if(hash.count(k-root->val))
-            return true;
-        hash[root->val] = 1;
-        
+        if(!root)                       return false;
+        if(hash.count(k- root->val))    return true;
+        hash.insert(root->val);
         if(!findTarget(root->left,k))
             return findTarget(root->right,k);
         return true;
     }
-​
 };
