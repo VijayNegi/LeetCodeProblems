12c1dd2801c18cfdb6115175c35259a0854b1598
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-10-10 12:18:55 +0530

    8 ms

---
+++ solutions/1328. Break a Palindrome.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string breakPalindrome1(string p) {
+        int n = p.size();
+        if(n<=1)
+            return "";
+        bool odd = n%2;
+        int i=0;
+        for(i=0;i<n;++i) {
+            if(odd && i==n/2)
+                continue;
+            if(p[i]!='a'){
+                p[i]='a';
+                break;
+            }  
+        }
+        if(i==n)
+            p.back()++;
+        return p;
+    }
+    string breakPalindrome(string S) {
+        int n = S.size();
+        for (int i = 0; i < n / 2; ++i) {
+            if (S[i] != 'a') {
+                S[i] = 'a';
+                return S;
+            }
+        }
+        S[n - 1] = 'b';
+        return n < 2 ? "" : S;
+    }
+};
