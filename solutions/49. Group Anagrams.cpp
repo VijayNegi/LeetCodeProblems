99637d2d6660a3e470ffe3a417196c9b50069e27
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-10-28 15:33:39 +0530

    89 ms

--- solutions/49. Group Anagrams.cpp
+++ solutions/49. Group Anagrams.cpp
@@ -1,16 +1,27 @@
 class Solution {
-    string strSort(string s) {
-        int counter[26] = {0};
-        for (char c : s) {
-            counter[c - 'a']++;
-        }
-        string t;
-        for (int c = 0; c < 26; c++) {
-            t.append(counter[c], c + 'a');
-        }
-        return t;
-    }
 public:
+    // new : 31 ms
+    vector<vector<string>> groupAnagrams1(vector<string>& strs) {
+        vector<vector<string>> result;
+        vector<pair<string,int>> sorted;
+        for(int i=0;i<strs.size();++i){
+            string s = strs[i];
+            sort(s.begin(),s.end());
+            sorted.push_back({s,i});
+        }
+        sort(sorted.begin(),sorted.end());
+        string pre="-";
+        for(auto& s:sorted){
+            if(s.first != pre)
+                result.push_back({strs[s.second]});
+            else
+                result.back().push_back(strs[s.second]);
+            pre = s.first;
+        }
+        return result;    
+    }
+    
+    // old solution
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         unordered_map<string,vector<string>> mAna;
         
@@ -25,5 +36,16 @@ public:
             result.push_back(vec.second);
         }
         return result;
+    }
+    string strSort(string s) {
+        int counter[26] = {0};
+        for (char c : s) {
+            counter[c - 'a']++;
+        }
+        string t;
+        for (int c = 0; c < 26; c++) {
+            t.append(counter[c], c + 'a');
+        }
+        return t;
     }
 };
