25a6d1282b12f0815c2968c9e9d19d889fe30b1c
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-11-06 12:11:32 +0530

    14 ms

---
+++ solutions/899. Orderly Queue.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    // self: 4 ms
+    string orderlyQueue1(string s, int k) {
+        int n = s.size();
+        string res = s;
+        if(k==1){
+            string temp = s +s;
+            for(int i=0;i<n;++i){
+                int flag = false;
+                for(int j=i;j<i+n;++j){
+                    if(res[j-i] < temp[j])
+                        break;
+                    else if(res[j-i] > temp[j]){
+                        flag = true;
+                        break;
+                    }
+                }
+                if(flag){
+                    res = temp.substr(i,n);
+                }
+            }
+        }
+        else{
+            sort(res.begin(),res.end());
+        }
+        return res;
+    }
+    // 14 ms
+     string orderlyQueue(string S, int K) {
+        if (K > 1) {
+            sort(S.begin(), S.end());
+            return S;
+        }
+        string res = S;
+        for (int i = 1; i < S.length(); i++)
+            res = min(res, S.substr(i) + S.substr(0, i));
+        return res;
+    }
+};
