40113419fd5e42ff94ebe561c278a1ab322747da
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-11-08 10:15:38 +0530

    5 ms

---
+++ solutions/1544. Make The String Great.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string makeGood(string s) {
+        int l=0,r=1;
+        while(r<s.size()) {
+            if(l<0)
+                s[++l] = s[r++];
+            else if((tolower(s[l]) == tolower(s[r])) && s[l]!=s[r]) 
+                       --l,++r;
+            else
+                   s[++l] = s[r++];
+        }
+        s.resize(l+1);
+        return s;
+    }
+};
