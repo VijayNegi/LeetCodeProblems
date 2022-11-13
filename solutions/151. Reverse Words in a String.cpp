821cd1ca30d560c8e941a7d1f009ddaa0c82a889
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-11-13 10:58:35 +0530

    3 ms

--- solutions/151. Reverse Words in a String.cpp
+++ solutions/151. Reverse Words in a String.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    string reverseWords(string s) {
+    // self : 21ms
+    string reverseWords1(string s) {
         stringstream ss(s);
         string result;
         string w;
@@ -10,5 +11,26 @@ public:
         }
         result.resize(result.size()-1);
         return result;
+    }
+    // inplace reverse
+    // 3ms
+    string reverseWords(string s) {
+        reverse(s.begin(), s.end());
+        int l = 0, r = 0, i = 0, n = s.size();
+        while (i < n) {
+            while (i < n && s[i] != ' ')
+                s[r++] = s[i++];
+​
+            if (l < r) { // if we can find a non-empty word then
+                reverse(s.begin() + l, s.begin() + r); // reverse current word
+                if (r == n) break;
+                s[r++] = ' '; // set empty space
+                l = r;
+            }
+            ++i; // now i == n or s[i] == ' ', so we skip that character!
+        }
+        if (r > 0 && s[r-1] == ' ') --r; // skip last empty character if have
+        s.resize(r);
+        return s;
     }
 };
