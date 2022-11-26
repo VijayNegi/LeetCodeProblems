1e01034d7a353b2593aef8b66a07db8842a13e6e
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-11-26 23:48:05 +0530

    1270 ms

---
+++ solutions/2484. Count Palindromic Subsequences.cpp
@@ -0,0 +1,62 @@
+const int mod = 1e9+7;
+#define ll long long
+class Solution {
+public:
+    // self : TLE
+    int countPalindromes1(string s) {
+        vector<vector<int>> stk(10);
+        int n = s.size();
+        for(int i=0;i<s.size();++i){
+            stk[s[i]-'0'].push_back(i);
+        }
+        int result = 0;
+        for(int i=0;i<n-4;++i){
+            for(int j=i+1;j<n-3;++j){
+                int fir = s[i]-'0';
+                int sec = s[j]-'0';
+                int r  = stk[fir].size()-1;
+​
+                for(;r>=0 && stk[fir][r]>j+1;--r){
+                    int rr = stk[sec].size()-1;
+                    while(rr>=0 && stk[sec][rr] >= stk[fir][r]) --rr;
+                    for(;stk[sec][rr]>j+1;--rr){
+                        result += (stk[sec][rr]-j)-1;
+                        result %=mod;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+    // o(1) space, from one contestent
+    // 828 ms
+    int countPalindromes2(string s) {
+            int n = s.size();
+            long ans = 0;
+            int ff[10] = {0};
+            for(int i = 0;i < n;i++){
+                long m = 0;
+                for(int j = n-1;j > i;j--){
+                    if(s[i] == s[j]){
+                        ans += m * (j-i-1);
+                    }
+                    m += ff[s[j]-'0'];
+                    if(m >= mod)m -= mod;
+                }
+                ff[s[i] - '0']++;
+            }
+            return (int)(ans % mod);
+    }
+    
+    ll dp[10001][11][11][6];
+    ll dfs(int ind, int first, int second, int i, string &s) {
+        if(i == 5) return 1; //found a subsequence
+        if(ind == s.length()) return 0;
+        if(dp[ind][first][second][i] != -1) return dp[ind][first][second][i];
+        
+        //option of not choosing current digit
+        long long res = dfs(ind + 1, first, second, i, s);
+​
+        if(i == 0) {
+            //option of choosing the first digit of the subsequence
+            res += dfs(ind + 1, s[ind] - '0', second, i + 1, s);
