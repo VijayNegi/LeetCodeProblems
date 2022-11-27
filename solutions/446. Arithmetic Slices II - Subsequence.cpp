39c187bc2f30040a5290e7f6fb73f8861d28af23
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-11-27 19:03:48 +0530

    736 ms

---
+++ solutions/446. Arithmetic Slices II - Subsequence.cpp
@@ -0,0 +1,38 @@
+#define LL long long
+class Solution {
+public:
+    // 886 ms
+    int numberOfArithmeticSlices1(vector<int>& A) {
+        int n = A.size();
+        LL ans = 0;
+        vector<map<LL, int>> cnt(n);
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                LL delta = (LL)A[i] - (LL)A[j];
+                int sum = 0;
+                if (cnt[j].find(delta) != cnt[j].end()) {
+                    sum = cnt[j][delta];
+                }
+                cnt[i][delta] += sum + 1;
+                ans += sum;
+            }
+        }
+​
+        return (int)ans;
+    }
+    // 736 ms
+    int numberOfArithmeticSlices(vector<int> &nums) {
+        int n = nums.size();
+        int ans = 0;
+        vector<unordered_map<long long, int>> dp(n); // dp[i][d]
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                long long diff = (long long) nums[i] - nums[j];
+                int cnt = dp[j].count(diff) ? dp[j][diff] : 0;
+                dp[i][diff] += cnt + 1;
+                ans += cnt;
+            }
+        }
+        return ans;
+    }
+};
