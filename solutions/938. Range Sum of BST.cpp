e82b1966b8ecfb5c26529cc2e9e43b4c83d34994
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-12-07 23:29:58 +0530

    326 ms

--- solutions/938. Range Sum of BST.cpp
+++ solutions/938. Range Sum of BST.cpp
@@ -12,17 +12,13 @@
 class Solution {
 public:
     int rangeSumBST(TreeNode* root, int low, int high) {
-        if(!root)
-            return 0;
-        int sum = 0;
-        if(root->val >=low && root->val <=high)
-            sum+=root->val;
-        
-        if(root->val > low)
-            sum += rangeSumBST(root->left, low , high);
-        if(root->val < high)
-            sum += rangeSumBST(root->right, low , high);
-        
-        return sum;
+        if(!root) return 0;
+        if(root->val >= low && root->val <= high)
+            return root->val + rangeSumBST(root->left,low,high)+ rangeSumBST(root->right,low,high);
+        if(root->val < low)
+            return rangeSumBST(root->right,low,high);
+        if(root->val > high)
+            return rangeSumBST(root->left,low,high);
+        return 0;
     }
 };
