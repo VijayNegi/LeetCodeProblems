0a4a1e640c34619b10ed22c94d7cc210f935beb8
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-12-09 00:53:50 +0530

    4 ms

--- solutions/872. Leaf-Similar Trees.cpp
+++ solutions/872. Leaf-Similar Trees.cpp
@@ -1,3 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool leafSimilar1(TreeNode* root1, TreeNode* root2) {
+        vector<TreeNode*> stk1,stk2;
+        function<void(vector<TreeNode*>&, TreeNode*)> pushLeft = [](vector<TreeNode*>& stk, TreeNode* node) {
+            while(node){
+                stk.push_back(node);
+                node = node->left;
+            }
+        };
+        pushLeft(stk1,root1);
         pushLeft(stk2,root2);
         
         while(stk1.size() && stk2.size()){
@@ -19,7 +41,7 @@
                 if(two->left != nullptr || two->right != nullptr){
                     stk2.pop_back();
                     pushLeft(stk2,two->right);
-                }
+                }
             }
         }
         while(stk1.size()){
@@ -27,15 +49,3 @@
             if(one->left == nullptr && one->right == nullptr )
                 return false;
             pushLeft(stk1,one->right);
-        }
-        while(stk2.size()){
-            TreeNode* one = stk2.back(); stk2.pop_back();
-            if(one->left == nullptr && one->right == nullptr )
-                return false;
-            pushLeft(stk2,one->right);
-        }
-            
-        return true;
-    }
-    
-};
