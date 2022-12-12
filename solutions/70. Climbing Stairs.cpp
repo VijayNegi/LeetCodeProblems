a8d3e33642d7f079c754d51b506ec46f0f158c86
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-12-12 20:16:25 +0530

    0 ms

--- solutions/70. Climbing Stairs.cpp
+++ solutions/70. Climbing Stairs.cpp
@@ -1,16 +1,10 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        if(n<3)
-            return n;
-        vector<int> dp(n+1,0);
-        dp[0]=1;
-        dp[1]=1;
-            
+        vector<int> steps(n+1,0);
+        steps[0]=steps[1]=1;
         for(int i=2;i<=n;++i)
-        {
-            dp[i] = dp[i-1] + dp[i-2];
-        }
-        return dp[n];
+            steps[i] = steps[i-1]+steps[i-2];
+        return steps[n];
     }
 };
