7b050695a940d100b141327234e23fe48f161510
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-12-12 20:34:10 +0530

    24 ms

---
+++ solutions/1. Two Sum.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> twoSum1(vector<int>& nums, int target) {
+        unordered_map<int,int> seen;
+        for(int i=0;i<nums.size();++i){
+            if(seen[target - nums[i]])
+                return {seen[target - nums[i]]-1,i};
+            seen[nums[i]] = i+1;
+        }
+        return {0,0};
+    }
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> hashmap;
+        for(int i=0;i<nums.size();++i)
+        {
+            auto it = hashmap.find(target - nums[i]);
+            if(it!=hashmap.end())
+                return {it->second,i};
+            hashmap[nums[i]] = i;
+        }
+        return {};
+    }
+};
