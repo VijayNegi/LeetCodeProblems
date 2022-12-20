fe75a20c7e9648882071973530302f7475163f84
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-12-20 21:11:30 +0530

    872 ms

---
+++ solutions/1971. Find if Path Exists in Graph.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    // can also be done with DSU
+    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
+        vector<vector<int>> adj(n);
+        for(auto& e:edges){
+            adj[e[0]].push_back(e[1]);
+            adj[e[1]].push_back(e[0]);
+        }
+            
+        vector<bool> visited(n,false);
+        queue<int> nodes;
+        nodes.push(source);
+        while(nodes.size()){
+            int node = nodes.front();
+            nodes.pop();
+            if(visited[node])
+                continue;
+            if(node == destination)
+                return true;
+            visited[node] = true;
+            for(auto& nei:adj[node])
+                if(!visited[nei])
+                    nodes.push(nei);
+        }
+        return false;
+    }
+};
