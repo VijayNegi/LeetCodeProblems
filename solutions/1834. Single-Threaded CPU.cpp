ff5026144bb52022289d0b1e0fd1d93ee1f64d9a
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2022-12-30 00:24:26 +0530

    841 ms

--- solutions/1834. Single-Threaded CPU.cpp
+++ solutions/1834. Single-Threaded CPU.cpp
@@ -1,5 +1,27 @@
-class Solution {
-public:
+        
+        long t = 0;
+        pq.push(tasks[0]);
+        t = tasks[0][0];
+        int i=1;
+        vector<int> result;
+        while(pq.size()){
+            auto tsk = pq.top();
+            pq.pop();
+            t += tsk[1];
+            result.push_back(tsk[2]);
+            while(i<n && t >= tasks[i][0]){
+                pq.push(tasks[i]);
+                ++i;
+            }
+            if(pq.empty() && i<n){
+                pq.push(tasks[i]);
+                t = tasks[i][0];
+                ++i;
+            }
+        }
+        return result;
+    }
+    // older solution 841 ms
     vector<int> getOrder(vector<vector<int>>& tasks) {
         
         auto cmp = [&](int left,int right) { return (tasks[left][0] > tasks[right][0]);};
@@ -13,30 +35,8 @@ public:
         long time = 0;
         while(!enque.empty() || !process.empty())
         {
+            
             // pop all jobs less then or equal then time from enque
             while(!enque.empty() && tasks[enque.top()][0] <= time)
             {
                 process.push(enque.top());
-                enque.pop();
-            }
-            
-            if(process.empty()) // not rechable if both are empty
-            {
-                time = tasks[enque.top()][0];
-               
-                while(!enque.empty() && tasks[enque.top()][0] <= time)
-                {
-                    process.push(enque.top());
-                    enque.pop();
-                }
-            }
-            
-            result.push_back(process.top());
-            time += tasks[process.top()][1];
-            process.pop();
-            
-        }
-        
-        return result;
-    }
-};
