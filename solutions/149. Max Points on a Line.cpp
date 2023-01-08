b4ade64ca45d4ede261fcfd9eccf22dcce8b6455
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-01-08 14:39:49 +0530

    54 ms

---
+++ solutions/149. Max Points on a Line.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    // official solution
+    int maxPoints1(vector<vector<int>>& points) {
+        int n = points.size();
+        if (n == 1) {
+            return 1;
+        }
+        int result = 2;
+        for (int i = 0; i < n; i++) {
+            unordered_map<double, int> cnt;
+            for (int j = 0; j < n; j++) {
+                if (j != i) {
+                    cnt[atan2(points[j][1] - points[i][1], points[j][0] - points[i][0])]++;
+                }
+            }
+            for (auto [h, count] : cnt) {
+                result = max(result, count + 1);
+            }
+        }
+        return result;
+    }
+    // https://leetcode.com/problems/max-points-on-a-line/discuss/47124/C%2B%2B-slope-counter
+    int maxPoints(vector<vector<int>>& points) {
+        int n = points.size(), ans = 0;
+        for (int i = 0; i < n; i++) {
+            unordered_map<string, int> counter;
+            int dup = 1;
+            for (int j = i + 1; j < n; j++) {
+                if (points[j][0] == points[i][0] && points[j][1] == points[i][1]) {
+                    dup++;
+                } else {
+                    int dx = points[j][0] - points[i][0], dy = points[j][1] - points[i][1], g = gcd(dx, dy);
+                    counter[to_string(dx / g) + '_' + to_string(dy / g)]++;
+                }
+            }
+            ans = max(ans, dup);
+            for (auto p : counter) {
+                ans = max(ans, p.second + dup);
+            }
+        }
+        return ans;
+    }
+    int gcd(int a, int b) {
+        while (b) {
+            a = a % b;
+            swap(a, b);
+        }
+        return a;
+    }
+};
