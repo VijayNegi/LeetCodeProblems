8b1e539f28b30a4e743a6d0d09a0263d80d18792
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-01-17 21:38:23 +0530

    25 ms

--- solutions/926. Flip String to Monotone Increasing.cpp
+++ solutions/926. Flip String to Monotone Increasing.cpp
@@ -1,59 +1,21 @@
 class Solution {
 public:
+    // self : 24ms
     int minFlipsMonoIncr1(string s) {
+        int result=0,ones=0;
+        for(auto& c:s)
+            ones += c=='1';
         int n = s.size();
-        vector<int> comm0(n+1,0);
-        vector<int> comm1(n+1,0);
-        
-        if(s[0]=='0')
-        {
-            comm0[0]++;
+        result = min(n-ones,ones); // either flip all ones or all zeros
+        int curr=0;
+        for(int i=0;i<n-1;++i){
+            curr += s[i]=='1';
+            result = min(result,curr +  n-1-i-ones+curr);// from current centre - flip all ones before and flip all zeros after 
         }
-        else
-            comm1[0]++;
-        
-        for(int i=1;i<n;++i)
-        {
-​
-            comm0[i] = comm0[i-1];
-            comm1[i] = comm1[i-1];
-            if(s[i]=='0')
-                comm0[i]++;
-            else
-                comm1[i]++;
-        }
-        int minF = n;
-        minF = min(minF, comm0[n-1]);
-        for(int i=1 ; i<n; ++i)
-        {
-            minF = min(minF, comm1[i-1] + comm0[n-1]- comm0[i-1]);
-        }
-        minF = min(minF, comm1[n-1]); 
-        return minF;
+        return result;
     }
-    int minFlipsMonoIncr2(string s) {
-        int n = s.size();
-        vector<int> comm1(n+1,0);
-        
-        if(s[0]=='1')
-            comm1[0]++;
-        
-        for(int i=1;i<n;++i)
-        {
-            comm1[i] = comm1[i-1];
-            if(s[i]=='1')
-                comm1[i]++;
-        }
-        int minF = n;
-        minF = min(minF, n - comm1[n-1]); // flip all zeros after
-        for(int i=1 ; i<n; ++i)
-        {
-            minF = min(minF, comm1[i-1] +  ((n-i) - (comm1[n-1] - comm1[i-1])));
-        }
-        minF = min(minF, comm1[n-1]); // flip all ones before
-        return minF;
-    }
-        int minFlipsMonoIncr(string S ) 
+    
+    int minFlipsMonoIncr(string S ) 
         {
             int counter_one  = 0,counter_flip = 0;
             for (auto ch : S) {
@@ -62,7 +24,7 @@ public:
             } else {
                 ++counter_flip;
             }
-            counter_flip = std::min(counter_one, counter_flip);
+            counter_flip = std::min(counter_one, counter_flip); // either we will have to flip all zeros before or all ones
         }
         return counter_flip;
     }
