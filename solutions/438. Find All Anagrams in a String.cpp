2a44e4cf9085b5323b3652ad45bc4994fb16e9f7
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-02-05 22:13:04 +0530

    16 ms

--- solutions/438. Find All Anagrams in a String.cpp
+++ solutions/438. Find All Anagrams in a String.cpp
@@ -1,25 +1,20 @@
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
-        vector<int> phist(26,0);
-        vector<int> thist(26,0);
-        for(auto c:p)
-            ++phist[c-'a'];
-​
-        int np = p.size();
-        int ns = s.size();
-        vector<int> res;
-        for(int i=0;i<ns;++i)
-        {
-            ++thist[s[i]-'a'];
-            if(i>=np-1)
-            {
-                if(i>=np)
-                    --thist[s[i-np]-'a'];
-                if(phist==thist)
-                    res.push_back(i-np+1);
-            }
+        int n = p.size();
+        vector<int> result;
+        if(n> s.size()) return result;
+        vector<int> hist1(26,0),hist2(26,0);
+        for(auto& c:p)
+            hist1[c-'a']++;
+        for(int i=0;i<n;++i)
+            hist2[s[i]-'a']++;
+        if(hist1==hist2) result.push_back(0);
+        for(int i=n;i<s.size();++i){
+            hist2[s[i]-'a']++;
+            hist2[s[i-n]-'a']--;
+            if(hist1==hist2) result.push_back(i-n+1);
         }
-        return res;
+        return result;
     }
 };
