8dc08474d1da04720d12e04218136a9b389c9958
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-02-07 22:43:01 +0530

    183 ms

--- solutions/904. Fruit Into Baskets.cpp
+++ solutions/904. Fruit Into Baskets.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
-    // self
     int totalFruit1(vector<int>& fruits) {
-        unordered_map<int,int> pick;
-        int count = 0;
-        int l=0,r=0,n=fruits.size();
-        int result = 0;
-        while(r<n){
-            while(r<n && (count<3)){
-                pick[fruits[r]]++;
-                if(pick[fruits[r]]==1) ++count;
-                ++r;
+        int result=0;
+        unordered_map<int,int> basket;
+        int start=0;
+        for(int i=0;i<fruits.size();++i){
+            basket[fruits[i]]++;
+            while(basket.size()>2){
+                basket[fruits[start]]--;
+                if(basket[fruits[start]]==0)
+                    basket.erase(fruits[start]);
+                ++start;
             }
-            result = max(result,r-l-(count==3));
-            while(l<r && count>2){
-                pick[fruits[l]]--;
-                if(pick[fruits[l]]==0) --count;
-                ++l;
-            }
+            result = max(result, i-start+1);
         }
         return result;
     }
