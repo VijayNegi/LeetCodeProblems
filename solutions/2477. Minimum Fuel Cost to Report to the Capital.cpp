d007a2a58deee117211cda20ec902ecbc8477617
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-02-12 14:50:58 +0530

    551 ms

---
+++ solutions/2477. Minimum Fuel Cost to Report to the Capital.cpp
@@ -0,0 +1,42 @@
+            auto node = leafs.front();
+            leafs.pop();
+            ++fuel;
+            fuel += (seat[node]-1)/seats;
+            for(auto nei:adj[node]){
+                inDegree[nei]--;
+                seat[nei]+= seat[node];
+                if(nei !=0 && inDegree[nei]==1){ // ignore capital
+                    leafs.push(nei);
+                }  
+            }
+            
+        }
+        return fuel;
+    }
+    // dfs
+    typedef long long ll;
+    long long minimumFuelCost(vector<vector<int>>& roads, int seats) {
+        int n = roads.size()+1;
+        //vector<int> inDegree(n,0);
+        vector<vector<int>> adj(n);
+        for(auto& r:roads){
+            adj[r[0]].push_back(r[1]);
+            adj[r[1]].push_back(r[0]);
+            //inDegree[r[0]]++;
+            //inDegree[r[1]]++;
+        }
+        ll fuel=0;
+        function<int(int,int)> dfs = [&](int node,int parent){
+            ll seat = 1;
+            for(auto child: adj[node]){
+                if(child!= parent)
+                    seat += dfs(child,node);
+            }
+            if(node!=0)
+                fuel += ceil((double)seat/seats);
+            return seat;
+        };
+        dfs(0,-1);
+        return fuel;
+    }
+};
