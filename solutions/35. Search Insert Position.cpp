66b4dbfb26c6a7116124b3fdf5a84c1afd047461
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-02-20 16:04:51 +0530

    10 ms

--- solutions/35. Search Insert Position.cpp
+++ solutions/35. Search Insert Position.cpp
@@ -1,22 +1,14 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        
-        int s = 0;
-        int e = nums.size() - 1;
-        
-        while(s<=e)
-        {
-            int mid = s + (e- s)/2;
-            
-            if(nums[mid]<target)
-                s = mid +1;
-            else if( nums[mid] > target)
-                e = mid - 1;
-            else
-                return mid;
-            
+        int l=0,r=nums.size();
+        while(l<r){
+            int mid = l+(r-l)/2;
+            if(nums[mid]>=target)
+                r = mid;
+            else if(nums[mid]<target)
+                l = mid +1;
         }
-        return s;
+        return l;
     }
 };
