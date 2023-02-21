2e7092ad43cda2f0ef890a2c7aad4a74b8c56cf7
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-02-21 16:46:49 +0530

    25 ms

--- solutions/540. Single Element in a Sorted Array.cpp
+++ solutions/540. Single Element in a Sorted Array.cpp
@@ -1,5 +1,31 @@
 class Solution {
 public:
+    //self
+    int singleNonDuplicate2(vector<int>& nums) {
+        int l,r,n;
+        n = nums.size();
+        l=0,r=n-1;
+        while(l<=r){
+            int mid = l + (r-l)/2;
+            if(mid%2){
+                if(mid>0 && nums[mid]==nums[mid-1]){
+                    l=mid+1;
+                }
+                else if(mid<n-1 && nums[mid]==nums[mid+1])
+                    r = mid-1;
+                else return nums[mid];
+            }
+            else {
+                if(mid<n-1 && nums[mid]==nums[mid+1]){
+                    l=mid+1;
+                }
+                else if(mid>0 && nums[mid]==nums[mid-1])
+                    r = mid-1;
+                else return nums[mid];
+            }
+        }
+        return -1;
+    }
     //xor
     int singleNonDuplicate1(vector<int>& nums) {
         int ans(0);
