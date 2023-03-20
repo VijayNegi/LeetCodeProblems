bcfdfcd41e8f2487444dd33da80dfbd2d6b12569
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-03-20 17:04:28 +0530

    12 ms

--- solutions/605. Can Place Flowers.cpp
+++ solutions/605. Can Place Flowers.cpp
@@ -1,5 +1,20 @@
 class Solution {
 public:
+    bool canPlaceFlowers1(vector<int>& fbed, int n) {
+        int k = fbed.size();
+        int result=0;
+        for(int i=0;i<k;i++){
+            if(!fbed[i]){
+                if(i>0 && fbed[i-1]) continue;
+                if(i<k-1 && fbed[i+1]) continue;
+                fbed[i]++;
+                ++result;
+            }
+        }
+        return result>=n;
+    }
+    // older solution
+    // calculate empty streach and then find how many plants we can fit in that streach
     bool canPlaceFlowers(vector<int>& flowerbed, int n) {
         int count = 0;
         int fn = flowerbed.size();
