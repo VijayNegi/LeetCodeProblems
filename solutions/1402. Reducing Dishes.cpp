25441f9cb38d6222a9cca6058317b201c984b197
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-03-29 18:21:17 +0530

    4 ms

---
+++ solutions/1402. Reducing Dishes.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    // n2 sol
+    int maxSatisfaction1(vector<int>& sat) {
+        sort(begin(sat),end(sat));
+        int result = 0,n=sat.size();
+        for(int i=0;i<n;++i){
+            int curr=0;
+            for(int j=i;j<n;++j){
+                curr += sat[j]*(j-i+1);
+            }
+            result = max(result,curr);
+        }
+        return result;
+    }
+    // nlog(n)
+    int maxSatisfaction(vector<int>& sat) {
+        sort(begin(sat),end(sat));
+        int result = 0,n=sat.size();
+        int pre=0;
+        for(int i=n-1;i>=0;--i){
+            if(i<n-1) sat[i] += sat[i+1];
+            pre += sat[i];
+            result = max(result,pre);
+        }
+        return result;
+    }
+};
