0cd26bc2a2478377144fd11c6ffc943312560544
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-04-11 23:08:59 +0530

    81 ms

---
+++ solutions/2390. Removing Stars From a String.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string removeStars(string s) {
+        int l=0,r=0,n=s.size();
+        for(int i=0;i<n;++i){
+            if(s[i]!='*')
+                s[l++]=s[i];
+            else
+                --l;
+        }
+        s.resize(l);
+        return s;
+    }
+};
