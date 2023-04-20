add781183c7887852c0f340d230d58b3a8c7ec3e
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-04-20 10:21:11 +0530

    11 ms

--- solutions/662. Maximum Width of Binary Tree.cpp
+++ solutions/662. Maximum Width of Binary Tree.cpp
@@ -1,3 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    // position index wise : RE
+    int widthOfBinaryTree(TreeNode* root) {
+        queue<pair<long,TreeNode*>> q;
+        q.push({0,root});
+        int level = 1;
+        long minw = INT_MAX;
+        long maxw = INT_MIN;
+        long result = 0;
+        long adj = 0;
+        while(!q.empty())
         {
             auto t = q.front();
             q.pop();
@@ -27,25 +50,3 @@
         vector<TreeNode*> q;
         q.push_back(root);
         int res = 0;
-        while(q.size()){
-            res = max(res,(int)q.size());
-            vector<TreeNode*> t;
-            for(auto& node:q){
-                if(!node && t.size()){
-                    t.push_back(nullptr);
-                    t.push_back(nullptr);
-                }
-                else if(node){
-                    if(node->left || t.size())
-                        t.push_back(node->left);
-                    if(node->right || t.size())
-                        t.push_back(node->right);
-                }
-            }
-            while(!t.empty() && !t.back())
-                t.pop_back();
-            swap(q,t);
-        }
-        return res;
-    }
-};
