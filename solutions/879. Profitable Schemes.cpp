5433348244c6a470265a344a2f8622355105c85c
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-04-21 23:37:55 +0530

    304 ms

--- solutions/879. Profitable Schemes.cpp
+++ solutions/879. Profitable Schemes.cpp
@@ -2,6 +2,7 @@ const int mod = 1e9+7;
 class Solution {
     int memo[101][101][101];
 public:
+    // official solution
     int profitableSchemes(int n, int minProfit, vector<int>& group, vector<int>& profit)    {
         int k = group.size();
         memset(memo, -1,sizeof(memo));
@@ -13,7 +14,7 @@ public:
             if(count + group[pos]<=n)
                 ways += dfs(pos+1,count+group[pos],min(minProfit, pro + profit[pos])); 
             return memo[pos][count][pro] = ways%mod;
-        };
+        };
         return dfs(0,0,0);
     }
 };
