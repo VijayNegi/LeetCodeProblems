3b2e0dfb226e841717346e2cbfe26fa2e385e3da
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-04-27 09:54:20 +0530

    0 ms

---
+++ solutions/319. Bulb Switcher.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int bulbSwitch1(int n) {
+        long k=1;
+        while(n>=k*k){
+            k++;
+        }
+        return k-1;
+    }
+    int bulbSwitch(int n) {
+        return sqrt(n);
+    }
+};
