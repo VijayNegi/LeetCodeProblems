10df12a10b7ed613d1f55bfa3344e518bcea2019
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-05-04 10:15:58 +0530

    8 ms

---
+++ solutions/649. Dota2 Senate.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    // self
+    string predictPartyVictory1(string senate) {
+        int rd=0,rr=0;
+        int s= senate.size();
+        while(true){
+            int l=0,r=0;
+            int ss =s;
+            while(r<ss){
+                if(senate[r]=='R'){
+                   if(rr) --rr,--s;
+                    else {
+                        senate[l++] = senate[r];
+                        ++rd;
+                    }
+                }
+                else{
+                    if(rd) --rd,--s;
+                    else {
+                        senate[l++] = senate[r];
+                        ++rr;
+                    }
+                }
+                ++r;
+            }
+            if(ss ==s)
+                break;
+        }
+        if(senate[0]=='R') return "Radiant";
+        return "Dire";
+    }
+    // Official solution
+    string predictPartyVictory(string senate) {
+​
+        int rCount = 0, dCount = 0;
+        int dFloatingBan = 0, rFloatingBan = 0;
+​
+        queue<char> q;
+        for (char c : senate) {
+            q.push(c);
+            if (c == 'R') rCount++;
+            else dCount++;
+        }
+​
+        while (rCount && dCount) {
+            char curr = q.front();
+            q.pop();
+​
+            if (curr == 'D') {
+                if (dFloatingBan) {
