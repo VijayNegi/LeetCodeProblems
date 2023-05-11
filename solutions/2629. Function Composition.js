09740182e1a38b46675c61973ed04b18a95a2287
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-05-11 15:09:21 +0530

    70 ms

---
+++ solutions/2629. Function Composition.js
@@ -0,0 +1,32 @@
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+var compose1 = function(functions) {
+    
+    return function(x) {
+        let val=x
+        for(let j=functions.length-1;j>=0;--j){
+            val = functions[j](val)
+        }
+        return val
+    }
+};
+var compose2 = function (functions) {
+  return function (x) {
+    let input = x;
+​
+    for (const func of functions.reverse()) {
+      input = func(input);
+    }
+​
+    return input;
+  };
+};
+​
+​
+​
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
