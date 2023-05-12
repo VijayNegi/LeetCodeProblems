a452fbaf4d4e780def7ca5c79c6a468e4b649a2b
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-05-12 23:52:11 +0530

    71 ms

---
+++ solutions/2666. Allow One Function Call.js
@@ -0,0 +1,44 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var once1 = function(fn) {
+    let k=1;
+    return function(...args){
+        if(k<2){
+            ++k
+            return fn(...args)
+        }
+        return undefined
+    }
+};
+var once2 = function(fn) {
+  let hasBeenCalled = false;
+  return function(...args){
+    if (hasBeenCalled) {
+      return undefined;
+    } else {
+      hasBeenCalled = true;
+      return fn(...args);
+    }
+  }
+};
+// Implicitly Return Undefined
+var once = function(fn) {
+  let hasBeenCalled = false;
+  return function(...args){
+    if (!hasBeenCalled) {
+      hasBeenCalled = true;
+      return fn(...args);
+    }
+  }
+};
+​
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
+​
