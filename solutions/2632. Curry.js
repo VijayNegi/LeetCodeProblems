fdaef18a4d67c862438c2476b81524acef846ba6
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-05-14 22:19:13 +0530

    74 ms

---
+++ solutions/2632. Curry.js
@@ -0,0 +1,29 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+// Currying with Recursive Function Calls
+var curry1 = function(fn) {
+    return function curried(...args) {
+        if(args.length >= fn.length)
+            return fn(...args)
+        return (...nextArgs) => curried(...args,...nextArgs)
+    };
+};
+// Currying with the Built-in Bind Method
+var curry = function (fn) {
+  return function curried(...args) {
+    if (args.length >= fn.length) {
+      return fn.apply(this, args);
+    }
+​
+    return curried.bind(this, ...args);
+  };
+};
+​
+/**
+ * function sum(a, b) { return a + b; }
+ * const csum = curry(sum);
+ * csum(1)(2) // 3
+ */
+​
