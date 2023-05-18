ac2183311dbb1f0e6e2b7f72eeeea429f4508961
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-05-18 23:56:27 +0530

    Update 2622. Cache With Time Limit.js

--- solutions/2622. Cache With Time Limit.js
+++ solutions/2622. Cache With Time Limit.js
@@ -1,63 +1,136 @@
-    this.cache[key] = { value, expiration: Date.now() + duration };
-    return hasUnexpiredValue;
+// Approach 2: setTimeout + clearTimeout + Function Syntax
+var TimeLimitedCache = function() {
+     this.cache = new Map();
+};
+
+/** 
+ * @param {number} key
+ * @param {number} value
+ * @param {number} time until expiration in ms
+ * @return {boolean} if un-expired key already existed
+ */
+TimeLimitedCache.prototype.set = function(key, value, duration) {
+    const valueInCache = this.cache.get(key);
+    if (valueInCache) {
+      clearTimeout(valueInCache.timeout);
+    }
+    const timeout = setTimeout(() => this.cache.delete(key), duration);
+    this.cache.set(key, { value, timeout });
+    return Boolean(valueInCache);
+};
+
+/** 
+ * @param {number} key
+ * @return {number} value associated with key
+ */
+TimeLimitedCache.prototype.get = function(key) {
+    return this.cache.has(key) ? this.cache.get(key).value : -1;
+};
+
+/** 
+ * @return {number} count of non-expired keys
+ */
+TimeLimitedCache.prototype.count = function() {
+    return this.cache.size;
+};
+
+/**
+ * Your TimeLimitedCache object will be instantiated and called as such:
+ * var obj = new TimeLimitedCache()
+ * obj.set(1, 42, 1000); // false
+ * obj.get(1) // 42
+ * obj.count() // 1
+ */
+
+// Approach 1: setTimeout + clearTimeout + Class Syntax
+class TimeLimitedCache1 {
+  cache = new Map();
+
+  set(key, value, duration) {
+    const valueInCache = this.cache.get(key);
+    if (valueInCache) {
+      clearTimeout(valueInCache.timeout);
+    }
+    const timeout = setTimeout(() => this.cache.delete(key), duration);
+    this.cache.set(key, { value, timeout });
+    return Boolean(valueInCache);
   }
-​
-  get(key) {
-    if (this.cache[key] === undefined) return -1;
-    if (Date.now() > this.cache[key].expiration) return -1;
-    return this.cache[key].value;
+
+  get(key) {
+    return this.cache.has(key) ? this.cache.get(key).value : -1;
   }
-​
-  count() {
-    let count = 0;
-    for (const entry of Object.values(this.cache)) {
-        if (Date.now() < entry.expiration) {
-            count += 1;
-        }
-    }
-    return count;
+
+  count() {
+    return this.cache.size;
+  }
+};
+
+// Approach 3: Maintain Expiration Times
+class TimeLimitedCache2 {
+  cache = {};
+
+  set(key, value, duration) {
+    const hasUnexpiredValue = key in this.cache && Date.now() < this.cache[key].expiration;
+    this.cache[key] = { value, expiration: Date.now() + duration };
+    return hasUnexpiredValue;
+  }
+
+  get(key) {
+    if (this.cache[key] === undefined) return -1;
+    if (Date.now() > this.cache[key].expiration) return -1;
+    return this.cache[key].value;
+  }
+
+  count() {
+    let count = 0;
+    for (const entry of Object.values(this.cache)) {
+        if (Date.now() < entry.expiration) {
+            count += 1;
+        }
+    }
+    return count;
   }
 };
 //  Approach 4: Maintain Expiration Times + Priority Queue
 class TimeLimitedCache3 {
-  cache = {};
-  queue = new MinPriorityQueue();
-  size = 0;
-​
-  handleExpiredData() {
-    const now = Date.now();
-    while (this.queue.size() > 0 && this.queue.front().priority < now) {
-      const entry = this.queue.dequeue().element;
-      if (!entry.overwritten) {
-        delete this.cache[entry.key];
-        this.size -= 1;
-      }
-    }
+  cache = {};
+  queue = new MinPriorityQueue();
+  size = 0;
+
+  handleExpiredData() {
+    const now = Date.now();
+    while (this.queue.size() > 0 && this.queue.front().priority < now) {
+      const entry = this.queue.dequeue().element;
+      if (!entry.overwritten) {
+        delete this.cache[entry.key];
+        this.size -= 1;
+      }
+    }
   };
-​
-  set(key, value, duration) {
-    this.handleExpiredData();
-    const hasVal = key in this.cache
-    if (hasVal) {
-      this.cache[key].overwritten = true
-    } else {
-      this.size += 1;
-    }
-    const expiration = Date.now() + duration;
-    const entry = { key, value, expiration, overwritten: false }
-    this.cache[key] = entry
-    this.queue.enqueue(entry, expiration);
-    return hasVal;
+
+  set(key, value, duration) {
+    this.handleExpiredData();
+    const hasVal = key in this.cache
+    if (hasVal) {
+      this.cache[key].overwritten = true
+    } else {
+      this.size += 1;
+    }
+    const expiration = Date.now() + duration;
+    const entry = { key, value, expiration, overwritten: false }
+    this.cache[key] = entry
+    this.queue.enqueue(entry, expiration);
+    return hasVal;
   }
-​
-  get(key) {
-    this.handleExpiredData();
-    if (this.cache[key] === undefined) return -1;
-    return this.cache[key].value;
+
+  get(key) {
+    this.handleExpiredData();
+    if (this.cache[key] === undefined) return -1;
+    return this.cache[key].value;
   }
-​
-  count() {
-    this.handleExpiredData();
-    return this.size;
+
+  count() {
+    this.handleExpiredData();
+    return this.size;
   }
 };
