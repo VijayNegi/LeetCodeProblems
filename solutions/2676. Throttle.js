cea6fc83b906f93dd19ce9d39bdb9c96470ee170
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-05-22 23:51:27 +0530

    54 ms

---
+++ solutions/2676. Throttle.js
@@ -0,0 +1,43 @@
+/**
+ * @param {Function} fn
+ * @param {number} t
+ * @return {Function}
+ */
+​
+// Approach 1: Recursive setTimeout Calls
+var throttle = function(fn, t) {
+  let timeoutInProgress = null;
+  let argsToProcess = null;
+  
+  const timeoutFunction = () => {
+    if (argsToProcess === null) {
+      timeoutInProgress = null; // enter the waiting phase
+    } else {
+      fn(...argsToProcess);
+      argsToProcess = null;
+      timeoutInProgress = setTimeout(timeoutFunction, t);
+    }
+  };
+​
+  return function throttled(...args) {
+    if (timeoutInProgress) {
+      argsToProcess = args;
+    } else {
+      fn(...args); // enter the looping phase
+      timeoutInProgress = setTimeout(timeoutFunction, t);
+    }
+  }
+};
+​
+// Approach 2: setInterval + clearInterval
+var throttle1 = function(fn, t) {
+  let intervalInProgress = null;
+  let argsToProcess = null;
+  
+  const intervalFunction = () => {
+    if (argsToProcess === null) {
+      clearInterval(intervalInProgress);
+      intervalInProgress = null; // enter the waiting phase
+    } else {
+      fn(...argsToProcess);
+      argsToProcess = null;
