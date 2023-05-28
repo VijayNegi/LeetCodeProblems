679d91d10a6df95f7d05913528f3e4f25e5f8bdb
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-05-28 18:16:24 +0530

    168 ms

---
+++ solutions/2631. Group By.js
@@ -0,0 +1,41 @@
+/**
+ * @param {Function} fn
+ * @return {Array}
+ */
+// self
+Array.prototype.groupBy1 = function(fn) {
+    let arr = {}
+    for(const ele of this){
+        const k = fn(ele)
+        if(!arr[k])
+            arr[k] = []
+        arr[k].push(ele)
+    }
+    return arr
+};
+Array.prototype.groupBy = function(fn) {
+    let arr = {}
+    for(const ele of this){
+        const k = fn(ele)
+        arr[k] ||= []; // Note
+        arr[k].push(ele)
+    }
+    return arr
+};
+​
+Array.prototype.groupBy2 = function(fn) {
+  const returnObject = {};
+  for (const item of this) {
+    const key = fn(item);
+    if (key in returnObject) {
+      returnObject[key].push(item);
+    } else {
+      returnObject[key] = [item];
+    }
+  }
+  return returnObject;
+};
+​
+/**
+ * [1,2,3].groupBy(String) // {"1":[1],"2":[2],"3":[3]}
+ */
