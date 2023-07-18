cc30e8db3be0ed0b507f54eee46e5b9592b6d136
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-07-18 10:19:12 +0530

    564 ms

--- solutions/146. LRU Cache.cpp
+++ solutions/146. LRU Cache.cpp
@@ -1,48 +1,44 @@
+};
+​
+#endif
 class LRUCache {
-    int size;
-    unordered_map<int,list<pair<int,int>>::iterator> cache;
-    list<pair<int,int>> used;
+    map<int,int> time2key;
+    unordered_map<int,pair<int,int>> key2timeval;
+    int time{0};
+    int maxsize;
 public:
     LRUCache(int capacity) {
-        size = capacity;
+        maxsize = capacity;
     }
     
     int get(int key) {
-        if(cache.count(key))
-        {
-            auto it = cache[key];
-            int value = (*it).second;
-            used.erase(it);
-            used.push_front({key,value});
-            cache[key] = used.begin();
-            return value;
-        }
-        return -1;
+        if(!key2timeval.count(key))
+            return -1;
+        auto& entry = key2timeval[key];
+        time2key.erase(entry.first);
+        entry.first = time++;
+        time2key[entry.first] = key;
+        return entry.second;
     }
     
     void put(int key, int value) {
-        // find key in map, yes, update it
-        // if not already in map, check size, 
-        // less then size put in cache
-        // size more then remove least recently use put.
-        if(cache.count(key))
-        {
-            used.erase(cache[key]);
-            used.push_front({key,value});
-            cache[key] = used.begin();
-            
+        if(key2timeval.count(key)){
+            auto& entry = key2timeval[key];
+            time2key.erase(entry.first);
+            entry.first = time++;
+            entry.second = value;
+            time2key[entry.first] = key;
         }
-        else
-        {
-            if(used.size()==size)
-            {
-                auto k = used.back();
-                used.pop_back();
-                cache.erase(k.first);
+        else{
+            pair<int,int> entry = {time++,value};
+            key2timeval[key] = entry;
+            time2key[entry.first] = key;
+            if(key2timeval.size()>maxsize){
+                auto itr = time2key.begin();
+                key2timeval.erase(itr->second);
+                time2key.erase(itr);
+                
             }
-            
-            used.push_front({key,value});
-            cache[key] = used.begin();
         }
     }
 };
