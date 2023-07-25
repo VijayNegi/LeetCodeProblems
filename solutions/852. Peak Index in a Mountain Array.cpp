c149899c1584b5b7de0dc1c7a087b95816158ef3
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-07-26 00:14:20 +0530

    137 ms

---
+++ solutions/852. Peak Index in a Mountain Array.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int peakIndexInMountainArray1(vector<int>& arr) {
+        int i = 0;
+        while (arr[i] < arr[i + 1]) {
+            i++;
+        }
+        return i;
+    }
+    // binary search
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int l = 0, r = arr.size() - 1, mid;
+        while (l < r) {
+            mid = (l + r) / 2;
+            if (arr[mid] < arr[mid + 1])
+                l = mid + 1;
+            else
+                r = mid;
+        }
+        return l;
+    }
+};
