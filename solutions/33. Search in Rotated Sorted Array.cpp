08b8034c39dfa9dbdb1ab5538defd03980ce8921
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-08-08 09:32:27 +0530

    3 ms

---
+++ solutions/33. Search in Rotated Sorted Array.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    // if target is in sorted half move to that otherwise move to unsorted half
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int left = 0, right = n - 1;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            
+            // Case 1: find target
+            if (nums[mid] == target) {
+                return mid;
+            }
+            
+            // Case 2: subarray on mid's left is sorted
+            else if (nums[mid] >= nums[left]) {
+                if (target >= nums[left] && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+            
+            // Case 3: subarray on mid's right is sorted
+            else {
+                if (target <= nums[right] && target > nums[mid]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+        
+        return -1;
+    }
+};
