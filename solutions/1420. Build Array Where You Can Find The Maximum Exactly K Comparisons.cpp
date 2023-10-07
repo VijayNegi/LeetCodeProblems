cdd19b30f71dbe7b6bcd104b3cb7524e8f0bcb8a
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-10-08 00:44:39 +0530

    156 ms

--- solutions/1420. Build Array Where You Can Find The Maximum Exactly K Comparisons.cpp
+++ solutions/1420. Build Array Where You Can Find The Maximum Exactly K Comparisons.cpp
@@ -8,12 +8,12 @@ public:
             if(i==n) return remainingk==0;
             if(dp[i][mmax][remainingk] != -1) return dp[i][mmax][remainingk];
             long result = dfs(i+1,mmax,remainingk);
-            result *=mmax;
+            result *=mmax; // for all values <= mmax result should be same
             result %=mod;
             for(int j=mmax+1;j<=m;++j){
                 result += dfs(i+1,j,remainingk-1);
                 result %=mod;
-            }
+            }
             return dp[i][mmax][remainingk] = result;
         };
         return dfs(0,0,k);
