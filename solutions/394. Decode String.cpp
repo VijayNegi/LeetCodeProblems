77fcda85b947e1b15e582192a9f83f6d739bdb11
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-10-24 12:42:05 +0530

    0 ms

--- solutions/394. Decode String.cpp
+++ solutions/394. Decode String.cpp
@@ -1,6 +1,36 @@
 class Solution {
 public:
+    // cleaner solution
+    string decodeString(const string& s, int& i) {
+        string res;
+        
+        while (i < s.length() && s[i] != ']') {
+            if (!isdigit(s[i]))
+                res += s[i++];
+            else {
+                int n = 0;
+                while (i < s.length() && isdigit(s[i]))
+                    n = n * 10 + s[i++] - '0';
+                    
+                i++; // '['
+                string t = decodeString(s, i);
+                i++; // ']'
+                
+                while (n-- > 0)
+                    res += t;
+            }
+        }
+        
+        return res;
+    }
+​
     string decodeString(string s) {
+        int i = 0;
+        return decodeString(s, i);
+    }
+​
+    // self
+    string decodeString1(string s) {
         string res;
         string temp;
         int i=0;
@@ -21,30 +51,5 @@ public:
             string t;
             while(i<s.size())
             {
+                //cout<<"s[i] "<<s[i]<<endl;
                 int x = getint(i);
-                if(x>0)
-                {
-                    ++i; // skip [
-                    string s = substr();
-                    while(x-- > 0)
-                        t.append(s);  
-                }
-                else if(s[i]>='a' && s[i]<='z')
-                {
-                    t.append(1,s[i]);
-                    ++i;               
-                }
-                else
-                {
-                    ++i;    // skip ]
-                    break;
-                }
-                    
-            }
-            return t;
-        };
-        
-        res = substr();
-        return res;
-    }
-};
