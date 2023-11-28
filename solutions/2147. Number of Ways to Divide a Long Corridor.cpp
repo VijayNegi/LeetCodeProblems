fde0b178914e5c4aa2a545e3c6578a75ab9b3ce9
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2023-11-28 23:25:34 +0530

    108 ms

--- solutions/2147. Number of Ways to Divide a Long Corridor.cpp
+++ solutions/2147. Number of Ways to Divide a Long Corridor.cpp
@@ -1,13 +1,3 @@
-const int mod = 1e9+7;
-class Solution {
-public:
-    int numberOfWays(string corridor) {
-        int n = corridor.size();
-        vector<int> dp(n,-1);
-        function<int(int)> dfs = [&](int k){
-            if(k>=n) return 0;
-            long long count=0;
-            if(dp[k]!=-1) return dp[k];
             int org = k;
             dp[org]=0;
             long long plant=0;
@@ -24,6 +14,40 @@ public:
         };
         
         return dfs(0);
-        
+    }
+    // same as above but without dp
+    int numberOfWays2(string s) {
+        long res = 1, j = 0, k = 0, mod = 1e9 + 7;
+        for (int i = 0; i < s.size(); ++i) {
+            if (s[i] == 'S') {
+                if (++k > 2 && k % 2 == 1)
+                    res = res * (i - j) % mod;
+                j = i;
+            }
+        }
+        return k % 2 == 0 && k > 0 ? res : 0;
+    }
+    // just dp optimization
+    int numberOfWays(string corridor) {
+        // Store 1000000007 in a variable for convenience
+        const int MOD = 1e9 + 7;
+​
+        // Initial values of three variables
+        int zero = 0;
+        int one = 0;
+        int two = 1;
+​
+        // Compute using derived equations
+        for (char thing : corridor) {
+            if (thing == 'S') {
+                zero = one;
+                swap(one, two);
+            } else {
+                two = (two + zero) % MOD;
+            }
+        }
+​
+        // Return the result
+        return zero;
     }
 };
