e58990c8b5b857711106935af839ab28b42d449d
Vijay Singh Negi <VijayNegi@users.noreply.github.com>
2024-05-09 09:38:05 +0530

    163 ms

---
+++ solutions/3075. Maximize Happiness of Selected Children.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    long long maximumHappinessSum(vector<int>& hp, int k) {
+        sort(begin(hp),end(hp),greater<>());
+        long res=0;
+        int sub=0;
+        for(int i=0;i<k;++i){
+            res += max(0,hp[i]-sub);
+            ++sub;
+            if(hp[i]<=sub)
+                break;
+        }
+        return res;
+    }
+};
